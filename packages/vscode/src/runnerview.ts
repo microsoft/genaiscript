a1cd54a6ebb4caa1426818dd9bd0d703424b0342
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-08-23 21:04:17 +0000

    towards a built view

--- packages/vscode/src/runnerview.ts
+++ packages/vscode/src/runnerview.ts
@@ -41,60 +41,30 @@ class RunnerViewProvider implements vscode.WebviewViewProvider {
 
     private _getHtmlForWebview(webview: vscode.Webview) {
         // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-        const scriptUri = getUri(webview, this.extensionUri, "media", "main.js")
-
-        // Do the same for the stylesheet.
-        const styleResetUri = getUri(
+        const webviewUri = getUri(
             webview,
             this.extensionUri,
-            "media",
-            "reset.css"
-        )
-        const styleVSCodeUri = getUri(
-            webview,
-            this.extensionUri,
-            "media",
-            "vscode.css"
-        )
-        const styleMainUri = getUri(
-            webview,
-            this.extensionUri,
-            "media",
-            "main.css"
+            "built",
+            "webview.js"
         )
 
         // Use a nonce to only allow a specific script to be run.
         const nonce = getNonce()
 
-        return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-
-				<!--
-					Use a content security policy to only allow loading styles from our extension directory,
-					and only allow scripts that have a specific nonce.
-					(See the 'webview-sample' extension sample for img-src content security policy examples)
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-				<link href="${styleMainUri}" rel="stylesheet">
-
-				<title>Cat Colors</title>
-			</head>
-			<body>
-				<ul class="color-list">
-				</ul>
-
-				<button class="add-color-button">Add Color</button>
-
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`
+        return `      <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'nonce-${nonce}';">
+            <title>Hello World!</title>
+          </head>
+          <body>
+            <h1>Hello World!</h1>
+                      <vscode-button id="howdy">Howdy!</vscode-button>
+                      <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
+          </body>
+        </html>`
     }
 }
 
