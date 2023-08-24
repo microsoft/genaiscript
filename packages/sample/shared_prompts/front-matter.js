8c17d588f4287898a2fc1aa560902e012cc8a848
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-08-24 19:39:04 +0000

    updated frontmatter example

---
+++ packages/sample/shared_prompts/front-matter.js
@@ -0,0 +1,19 @@
+prompt({
+    title: "Title and Description",
+    replaces: "fragment",
+    categories: ["style"],
+    system: []
+})
+
+$`
+You are a helpful front matter generator. You are an SEO expert.
+`
+
+def("SOURCE", env.file)
+
+$`Update or generate the front matter in SOURCE. Minimize changes.`
+$`- Update fields title, description and keywords
+- use yaml format, do not use quotes
+- only 5 keywords or less
+- optimize for search engine optimization.
+- Do not modify the markdown content after the front matter.`
