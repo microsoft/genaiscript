2361fabf89e17af70124857b8a650a3d7703d0ed
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-08-31 15:02:13 +0000

    added system.notes in sde-coing

--- packages/mywordle/prompts/SDE-coding.prompt.js
+++ packages/mywordle/prompts/SDE-coding.prompt.js
@@ -3,7 +3,7 @@ prompt({ title: "SDE-coding",
          outputFolder: "src",
          maxTokens: 4000,
          model: "gpt-4-32k",
-         system: ["system.code", "system.concise", "system.multifile"],
+         system: ["system.code", "system.concise", "system.multifile", "system.notes"],
          categories: ["appdev"]  })
 
 def("SUMMARY", env.subtree)
