989cbfa686ce75aab9f6934221acc3d088f09121
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-08-31 14:59:56 +0000

    updated system.notes

--- packages/core/src/prompts/system.notes.prompt.js
+++ packages/core/src/prompts/system.notes.prompt.js
@@ -1,5 +1,5 @@
 systemPrompt({ title: "Setup for explanation notes" })
 
-$`When explation to answers or septs, use this syntax:`
+$`Write an explanation of your answers and use this syntax:`
 
 def(`Note`, `This is why I generated this.`)
