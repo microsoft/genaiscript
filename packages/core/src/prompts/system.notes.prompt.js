64a86126333bc42cfac627fab271cef9ee30143f
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-08-31 15:44:21 +0000

    updated prompt

--- packages/core/src/prompts/system.notes.prompt.js
+++ packages/core/src/prompts/system.notes.prompt.js
@@ -1,5 +1,5 @@
 systemPrompt({ title: "Setup for explanation notes" })
 
-$`Write an explanation of your answers and use this syntax:`
+$`Write an explanation of your answers in markdown and use this syntax:`
 
 def(`Note`, `This is why I generated this.`)
