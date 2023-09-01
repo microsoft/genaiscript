f35ebada4d36088283f01443fcf499aeea78cf62
Ben Zorn <zorn@microsoft.com>
2023-08-31 17:05:00 -0700

    Fixed a bug in references to the code

--- packages/mywordle/prompts/SDE-update.prompt.js
+++ packages/mywordle/prompts/SDE-update.prompt.js
@@ -1,5 +1,5 @@
 prompt({ title: "SDE-update", 
-         description: "Based on changes to the spec and a bugnotes.md file, updates code for an application based on a specification of the specific files and APIs provided in a .saplan.coarch.md file",
+         description: "Updates code for an application based on a specification of the specific files and APIs provided in a .saplan.coarch.md file",
          maxTokens: 4000,
          outputFolder: "src",
          model: "gpt-4-32k",
@@ -7,8 +7,8 @@ prompt({ title: "SDE-update",
          categories: ["appdev"]  })
 
 def("SUMMARY", env.subtree)
-def("CODE", env.output)
-def("DOCS", env.links.filter(f => f.filename.endsWith(".md")))
+def("CODE", env.links.filter(f => f.filename.endsWith(".py")))
+def("DOCS", env.links.filter(f => f.filename.endsWith(".coarch.md")))
 def("BUGS", env.links.filter(f => f.filename.startsWith("bug")))
 
 $`
@@ -18,17 +18,18 @@ You are an expert software developer with years of experience implementing Pytho
 You always write syntactically correct code that is easy to read and understand. 
  
 The resulting CODE should be complete.  
-Do not leave any incomplete content as a work item todo in a comment.
-Do not generate comments of the form "Implement xxxx here" or "Implement xxxx later".
 
 A software architect has specified the architecture for a new product 
 and has defined the APIs for each component in SUMMARY.   
 CODE contains the code for the product generated from a previous SUMMARY.
 
 Issues with the current implementation have been described in BUGS.
+Issues that have already been resolved are in a section called Past Issues and 
+issues that you need to resolve are in a section called New Issues.
+Address only the new issues.  Do not make changes related to the Past Issues.
 
 Your job is to make a minimum of updates to CODE to match any changes that have 
-made to SUMMARY and to address any issues mentioned in BUGS.
+made to SUMMARY and to address any new issues mentioned in BUGS.
 
 For each of the Python files listed in SUMMARY, CODE contains the code for
 the component which is in a separate file using the file name used in SUMMARY.
@@ -38,19 +39,8 @@ address any updates to SUMMARY or any issues mentioned in BUGS.
 
 Modify as few files as possible and only generate the code for the files that need to be changed.
 
-Make sure that the code is well documented and that the code is easy to read and understand.
-Make sure that the comments follow the Python commenting conventions.
-Make sure that the code follows all the APIs specified in SUMMARY.
-Make sure that the code is modular and that a quality assurance engineer can 
-write test cases for each component.
-Make sure that you can run the client component on the command line for demonstration and testing purposes.
-Include assertions in your code to ensure that the code is correct.
-
 Respond only with the new CODE.
 
-Limit changes to existing code to minimum.  Avoid making changes that only change the method or variable names.
-For example avoid renaming ui.display_illegal_word() to ui.display_illegal_word_message()
-and avoid renaming ui.display_illegal_word() to ui.display_incorrect_word() if no other changes are required.
+Limit changes to existing code to minimum. 
 
-Always ensure that code you generate is well-formed Python code that can be run.  
 `
