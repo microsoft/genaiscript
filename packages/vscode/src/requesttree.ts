456b6180a929a7c7b3b95a71f310c132a6886176
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-09-05 19:05:51 +0000

    boiler plate code to get a view of previous requests

---
+++ packages/vscode/src/requesttree.ts
@@ -0,0 +1,78 @@
+import * as vscode from "vscode"
+import {
+    AI_REQUEST_CHANGE,
+    ExtensionState,
+    FRAGMENTS_CHANGE,
+    FragmentsEvent,
+} from "./state"
+import {
+    CHANGE,
+    CacheEntry,
+    Fragment,
+    allChildren,
+    concatArrays,
+    getChatCompletionCache,
+} from "coarch-core"
+import type { CreateChatCompletionRequest } from "openai"
+import { Cache } from "coarch-core"
+
+type RequestTreeNode = CacheEntry<CreateChatCompletionRequest, string>
+
+class RequestTreeDataProvider
+    implements vscode.TreeDataProvider<RequestTreeNode>
+{
+    cache: Cache<CreateChatCompletionRequest, string>
+    constructor(readonly state: ExtensionState) {
+        this.cache = getChatCompletionCache()
+        this.cache.addEventListener(CHANGE, () => this.refresh())
+    }
+
+    async getTreeItem(element: RequestTreeNode): Promise<vscode.TreeItem> {
+        const { sha, key, val } = element
+        const item = new vscode.TreeItem(
+            sha,
+            vscode.TreeItemCollapsibleState.None
+        )
+        item.id = sha
+        return item
+    }
+
+    async getChildren(
+        element?: RequestTreeNode | undefined
+    ): Promise<RequestTreeNode[]> {
+        if (!element) {
+            const entries = await this.cache.entries()
+            return entries
+        }
+        return undefined
+    }
+
+    async resolveTreeItem?(
+        item: vscode.TreeItem,
+        element: RequestTreeNode,
+        token: vscode.CancellationToken
+    ) {
+        return item
+    }
+
+    private _onDidChangeTreeData: vscode.EventEmitter<
+        void | RequestTreeNode | RequestTreeNode[]
+    > = new vscode.EventEmitter<void | RequestTreeNode | RequestTreeNode[]>()
+    readonly onDidChangeTreeData: vscode.Event<
+        void | RequestTreeNode | RequestTreeNode[]
+    > = this._onDidChangeTreeData.event
+
+    refresh(treeItem?: RequestTreeNode | RequestTreeNode[]): void {
+        this._onDidChangeTreeData.fire(treeItem)
+    }
+}
+
+export function activateRequestTreeDataProvider(state: ExtensionState) {
+    const { context } = state
+    const { subscriptions } = context
+    const treeDataProvider = new RequestTreeDataProvider(state)
+    const treeView = vscode.window.createTreeView("coarch.openai.requests", {
+        treeDataProvider,
+    })
+    subscriptions.push(treeView)
+}
