f4a6b8173fb8ab6c3eef0f082a1cf85bf11a3722
Ben Zorn <zorn@microsoft.com>
2023-09-07 09:15:03 -0700

    developing small example

---
+++ packages/helloworld/prompts/generate python tests.prompt.js
@@ -0,0 +1,20 @@
+prompt({
+    title: "↓ generate python tests",
+    output: ".tests.py",
+    model: "gpt-4-32k",
+    replaces: "nothing",
+    description: "Given a task and code, generate tests",
+    categories: ["tutorial"],
+})
+
+def("TESTS", env.output)
+def("TASK", env.children)
+def("CODE", env.links.filter(f => f.filename.endsWith(".py")))
+
+$`Python has been written for the task in TASK. The code is in CODE.
+Generate 5 tests for the code in CODE.
+If the tests are already present in TESTS, ensure that the tests
+match the description in TASK and the code in CODE. 
+
+Include a test harness that can run the tests from the command line
+Ensure that the result is well-formed Python code`
