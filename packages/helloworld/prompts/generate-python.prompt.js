5a1b2ebe7b3138d8e455f4fb14f2976c7769641c
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-06 13:13:06 -0700

    Clipboard (#91)
    * declare clipboard support
    
    * updated prompts to use clipboard

--- packages/helloworld/prompts/generate-python.prompt.js
+++ packages/helloworld/prompts/generate-python.prompt.js
@@ -10,9 +10,12 @@ def("CODE", env.links.filter(
     (f) => f.filename.endsWith(".py") && !f.filename.startsWith("test_")
 ))
 def("TASK", env.file)
+def("CLIPBOARD", env.clipboard)
 
 $`You are an expert python programmer.
 Generate python code in CODE for the task in TASK.
+
+If CLIPBOARD is present, analyze it for runtime errors and fix the code.
  
 If the CODE is already present, ensure that CODE matches the
 description in TASK and make minimal changes if it does not.`
