b95fd0db1837a53491fe3661548f9895c292874b
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-06 11:18:23 -0700

    updated hello world sample (#89)

---
+++ packages/helloworld/test_email_recognizer.py
@@ -0,0 +1,21 @@
+import unittest
+from email_recognizer import is_valid_email_or_url
+
+class TestEmailRecognizer(unittest.TestCase):
+    def test_valid_email(self):
+        self.assertTrue(is_valid_email_or_url("user@example.com"))
+
+    def test_invalid_email(self):
+        self.assertFalse(is_valid_email_or_url("user@.com"))
+
+    def test_valid_url(self):
+        self.assertTrue(is_valid_email_or_url("https://www.example.com"))
+
+    def test_invalid_url(self):
+        self.assertFalse(is_valid_email_or_url("htp://www.example.com"))
+
+    def test_mixed_input(self):
+        self.assertFalse(is_valid_email_or_url("user@example.comhttps://www.example.com"))
+
+if __name__ == '__main__':
+    unittest.main()
