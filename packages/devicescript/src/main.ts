d021e2056ba50db79ae7f1b5067c19c2b2df9bb5
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-09 22:32:09 +0000

    updated generated code.

--- packages/devicescript/src/main.ts
+++ packages/devicescript/src/main.ts
@@ -1,10 +1,11 @@
-import { PCF8563 } from "./pcf8563";
+import { pins, board } from "@dsboard/seeed_xiao_esp32c3"
+import { PCF8563 } from "./pcf8563"
 
-const rtc = new PCF8563();
+const rtc = new PCF8563()
 
 async function displayTime() {
-  const currentTime = await rtc.readTime();
-  console.log(`Current time: ${currentTime}`);
+    const currentTime = await rtc.readTime()
+    console.log(`Current time: ${currentTime}`)
 }
 
-await displayTime();
+await displayTime()
