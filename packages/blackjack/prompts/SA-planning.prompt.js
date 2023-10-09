8d3c522c1006b1ae5121e0087650acbe2164c8ff
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-09 20:23:31 +0000

    updated jacdac prompts

--- packages/blackjack/prompts/SA-planning.prompt.js
+++ packages/blackjack/prompts/SA-planning.prompt.js
@@ -1,17 +1,18 @@
-prompt({ title: "SA-planning", 
-         output: ".saplan.coarch.md", 
-         maxTokens: 4000,
-         model: "gpt-4-32k",
-         categories: ["appdev"]  })
+prompt({
+    title: "Software Architect - planning",
+    description: "A software architect generates instructions for the software engineer",
+    categories: ["appdev"]
+})
 
-def("SUMMARY", env.subtree)
-def("CODE", env.output)
+const output = env.file.filename?.replace(/\.coarch\.md/, ".saplan.coarch.md")
+def("FILE", env.file)
+def("CODE", env.links.filter(f => f.filename === output))
 
 $`
-You are an expert software architect.
+You are an expert Python software architect.
 You have been given a product idea and your job is to define a plan to implement the product.
 The instructions contain the high level idea and specific directions from the product manager for you to follow.
-Use the SUMMARY as input and generate the architecture for the product.
+Use the SPEC as input and generate the architecture for the product.
 
 Encapsulate each component in a separate module and define the APIs for each component.
 Make sure that the components are loosely coupled, they can be easily tested and that the APIs are well defined.
@@ -23,7 +24,7 @@ Separate sections of your output into markdown subsections.
 
 First, choose a programming language and a framework to implement the product.
 Next, show the directory structure for the code using bullet points in markdown.
-Next include the instructions for the software developer from SUMMARY in your output and
+Next include the instructions for the software developer from SPEC in your output and
 Next make suggestions for invariants that are related to the application domain.  For example,
 for the game of blackjack, the player or the dealer cannot win if the sum of their cards is greater than 21.
 
@@ -35,7 +36,7 @@ Also, define a command line client that will use the product that can be used bo
 Make sure that the client is easy to use and that it is well documented.
 Make sure that your instructions are clear, concise, and unambiguous.
 
-Respond with the new CODE.
+Respond with the new CODE in ${output}.
 
 Limit changes to CODE.
 Generate markdown, do not generate Python code.
