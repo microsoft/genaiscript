b5801c89322879ea8b6bf936059b1df2e5f901f9
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-09 23:56:05 +0000

    updated devicescript example

--- packages/devicescript/src/main.ts
+++ packages/devicescript/src/main.ts
@@ -1,10 +1,13 @@
+import { pins, board } from "@dsboard/seeed_xiao_esp32c3"
 import { PCF8563 } from "./pcf8563";
+import { XiaoExpansionBoard } from "@devicescript/drivers";
 
+const shield = new XiaoExpansionBoard()
+const rtc = new PCF8563();
+await rtc.init();
 async function main() {
-  const rtc = new PCF8563();
-  await rtc.init();
   const currentTime = await rtc.readTime();
-  console.log("Current time:", currentTime);
+  console.log(currentTime);
 }
 
-await main();
+setInterval(main, 1000)
--- packages/devicescript/src/pcf8563.ts
+++ packages/devicescript/src/pcf8563.ts
@@ -1,36 +1,41 @@
-import { I2CDriver } from "@devicescript/drivers";
+import { I2CDriver } from "@devicescript/drivers"
 
-const PCF8563_ADDR = 0x51;
+const PCF8563_ADDR = 0x51
 
-const REG_CTRL1 = 0x00;
-const REG_CTRL2 = 0x01;
-const REG_TIME = 0x02;
+const REG_CTRL1 = 0x00
+const REG_CTRL2 = 0x01
+const REG_TIME = 0x02
 
 export class Date {
-  constructor(year: number, month: number, day: number, hours: number, minutes: number, seconds: number) {
-
-  }
+    constructor(
+        public readonly year: number,
+        public readonly month: number,
+        public readonly day: number,
+        public readonly hours: number,
+        public readonly minutes: number,
+        public readonly seconds: number
+    ) {}
 }
 
 export class PCF8563 extends I2CDriver {
-  constructor(addr: number = PCF8563_ADDR) {
-    super(addr);
-  }
+    constructor(addr: number = PCF8563_ADDR) {
+        super(addr)
+    }
 
-  protected async initDriver(): Promise<void> {
-    await this.writeReg(REG_CTRL1, 0x00);
-    await this.writeReg(REG_CTRL2, 0x00);
-  }
+    protected async initDriver(): Promise<void> {
+        await this.writeReg(REG_CTRL1, 0x00)
+        await this.writeReg(REG_CTRL2, 0x00)
+    }
 
-  async readTime(): Promise<Date> {
-    const timeBuf = await this.readRegBuf(REG_TIME, 7);
-    const year = timeBuf[6] + 2000;
-    const month = timeBuf[5] & 0x1f;
-    const day = timeBuf[3] & 0x3f;
-    const hours = timeBuf[2] & 0x3f;
-    const minutes = timeBuf[1] & 0x7f;
-    const seconds = timeBuf[0] & 0x7f;
+    async readTime(): Promise<Date> {
+        const timeBuf = await this.readRegBuf(REG_TIME, 7)
+        const year = timeBuf[6] + 2000
+        const month = timeBuf[5] & 0x1f
+        const day = timeBuf[3] & 0x3f
+        const hours = timeBuf[2] & 0x3f
+        const minutes = timeBuf[1] & 0x7f
+        const seconds = timeBuf[0] & 0x7f
 
-    return new Date(year, month - 1, day, hours, minutes, seconds);
-  }
+        return new Date(year, month - 1, day, hours, minutes, seconds)
+    }
 }
