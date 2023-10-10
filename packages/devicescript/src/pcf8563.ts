61fd0d69ce326730cd9afad2408d19a5bcaa2691
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-10 16:41:11 +0000

    mroe experiments

--- packages/devicescript/src/pcf8563.ts
+++ packages/devicescript/src/pcf8563.ts
@@ -1,45 +1,50 @@
 import { I2CDriver } from "@devicescript/drivers"
+import { Date } from "./date"
 
-const PCF8563_ADDR = 0x51
+const PCF8563_I2C_ADDR = 0x51 // Default I2C address from datasheet
 
-const REG_CTRL1 = 0x00
-const REG_CTRL2 = 0x01
-const REG_TIME = 0x02
-
-export class Date {
-    constructor(
-        public readonly year: number,
-        public readonly month: number,
-        public readonly day: number,
-        public readonly hours: number,
-        public readonly minutes: number,
-        public readonly seconds: number
-    ) {}
-}
+const REG_SECONDS = 0x02 // Register address for SECONDS from datasheet
 
 export class PCF8563 extends I2CDriver {
-    constructor(addr: number = PCF8563_ADDR) {
-        super(addr)
+    constructor(i2cAddress: number = PCF8563_I2C_ADDR) {
+        super(i2cAddress)
     }
 
     protected async initDriver(): Promise<void> {
-        await this.writeReg(REG_CTRL1, 0x00)
-        await this.writeReg(REG_CTRL2, 0x00)
+        // Initialization sequence to set control registers
+        await this.writeReg(0x00, 0x00) // Control1 register: disable all alarms and interrupts
+        await this.writeReg(0x01, 0x00) // Control2 register: disable all alarms and interrupts
     }
 
     async readTime(): Promise<Date> {
-        const timeBuf = await this.readRegBuf(REG_TIME, 7)
-        const year = timeBuf[6] + 2000
-        const month = timeBuf[5] & 0x1f
-        const day = timeBuf[3] & 0x3f
-        const hours = timeBuf[2] & 0x3f
-        const minutes = timeBuf[1] & 0x7f
-        const seconds = timeBuf[0] & 0x7f
+        const buf = await this.readRegBuf(REG_SECONDS, 7)
+        const seconds = this.bcdToDecimal(buf[0] & 0x7f)
+        const minutes = this.bcdToDecimal(buf[1] & 0x7f)
+        const hours = this.bcdToDecimal(buf[2] & 0x3f)
+        const days = this.bcdToDecimal(buf[3] & 0x3f)
+        const months = this.bcdToDecimal(buf[5] & 0x1f)
+        const years = this.bcdToDecimal(buf[6]) + 2000
 
-        return new Date(year, month - 1, day, hours, minutes, seconds)
+        return new Date(years, months - 1, days, hours, minutes, seconds)
     }
 
-    async writeTime(date: Date) {
-        // TODO: write date to REG_TIME register
+    async writeTime(date: Date): Promise<void> {
+        const buf = Buffer.alloc(7)
+        buf[0] = this.decimalToBcd(date.seconds)
+        buf[1] = this.decimalToBcd(date.minutes)
+        buf[2] = this.decimalToBcd(date.hours)
+        buf[3] = this.decimalToBcd(date.day)
+        buf[5] = this.decimalToBcd(date.month + 1)
+        buf[6] = this.decimalToBcd(date.year % 100)
+
+        await this.writeRegBuf(REG_SECONDS, buf)
+    }
+
+    private bcdToDecimal(bcd: number): number {
+        return (bcd >> 4) * 10 + (bcd & 0x0f)
+    }
+
+    private decimalToBcd(decimal: number): number {
+        return ((decimal / 10) << 4) | decimal % 10
     }
 }
