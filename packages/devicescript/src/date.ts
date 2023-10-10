164f1823d64d89454b1899700d13aadbaf3ab817
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-10 16:41:11 +0000

    mroe experiments

---
+++ packages/devicescript/src/date.ts
@@ -0,0 +1,10 @@
+export class Date {
+    constructor(
+        public readonly year: number,
+        public readonly month: number,
+        public readonly day: number,
+        public readonly hours: number,
+        public readonly minutes: number,
+        public readonly seconds: number
+    ) {}
+}
