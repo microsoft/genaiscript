13ec3c140e6baf7ceb3db119fb9a8772dabdfb9a
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-10 22:24:40 +0000

    generating some kind of diff

--- packages/devicescript/src/pcf8563.ts
+++ packages/devicescript/src/pcf8563.ts
@@ -17,27 +17,11 @@ export class PCF8563 extends I2CDriver {
     }
 
     async readTime(): Promise<Date> {
-        const buf = await this.readRegBuf(REG_SECONDS, 7)
-        const seconds = this.bcdToDecimal(buf[0] & 0x7f)
-        const minutes = this.bcdToDecimal(buf[1] & 0x7f)
-        const hours = this.bcdToDecimal(buf[2] & 0x3f)
-        const days = this.bcdToDecimal(buf[3] & 0x3f)
-        const months = this.bcdToDecimal(buf[5] & 0x1f)
-        const years = this.bcdToDecimal(buf[6]) + 2000
-
-        return new Date(years, months - 1, days, hours, minutes, seconds)
+        // TODO
     }
 
     async writeTime(date: Date): Promise<void> {
-        const buf = Buffer.alloc(7)
-        buf[0] = this.decimalToBcd(date.seconds)
-        buf[1] = this.decimalToBcd(date.minutes)
-        buf[2] = this.decimalToBcd(date.hours)
-        buf[3] = this.decimalToBcd(date.day)
-        buf[5] = this.decimalToBcd(date.month + 1)
-        buf[6] = this.decimalToBcd(date.year % 100)
-
-        await this.writeRegBuf(REG_SECONDS, buf)
+        // TODO
     }
 
     private bcdToDecimal(bcd: number): number {
