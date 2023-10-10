db3ed1501a50638ded142945c81f7f00b72783c3
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-10 21:16:26 +0000

    better handling of subfolders

--- packages/core/src/prompts/system.prompt.js
+++ packages/core/src/prompts/system.prompt.js
@@ -2,17 +2,17 @@ systemPrompt({ title: "System prompt" })
 
 const folder =
     env.vars["system.multifile.outputFolder"] ||
-    env.template.outputFolder ||
-    "."
+    env.template.outputFolder
 $`You are concise. Answer in markdown.
 
-When generating files you will use the following syntax:`
+When generating or updating files you will use the following syntax:`
 
-def(`File ${folder}/file1.ts`, `What goes in\n${folder}/file1.ts.`)
-def(`File ${folder}/file2.md`, `What goes in\n${folder}/file2.md.`)
+def(`File ${folder || "."}/file1.ts`, `What goes in\n${folder || "."}/file1.ts.`)
+def(`File /path_to_file/file2.md`, `What goes in\n/path_to_file/file2.md.`)
 
 $`Make sure to use precisely ${env.fence} to guard file code sections.`
-
-$`When generating files, place files in folder "${folder}". Do not respond unchanged files.`
-
+$`Use full path of filename in code section header.`
+if (folder)
+    $`When generating new files, place files in folder "${folder}".`
+$`If a file does not have changes, do not regenerate.`
 $`When explaining answers, take a deep breath.`
