bfe0be8b5a4cd491e6d799791ccff0ae62d57a5e
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-10 22:09:54 +0000

    added system.typescript, system.python

---
+++ packages/core/src/prompts/system.python.prompt.js
@@ -0,0 +1,6 @@
+systemPrompt({
+    title: "Python Developer",
+    description: "Expert at generating and understanding Python code."
+})
+
+$`Also, you are an expert coder in Python. You create code that is PEP8 compliant.`
