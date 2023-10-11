8ad34c50c85b4df4e209373e4f05e425e4ff9bc4
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-11 15:55:29 +0000

    udpated devicescript

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -2,28 +2,47 @@ import { readFileSync, writeFileSync } from "fs"
 import { tokenize } from "./jstokenizer"
 import { getSubtasks } from "./refinement"
 
-import { parseProject } from "coarch-core"
+import { extractFenced, parseProject } from "coarch-core"
 import { NodeHost } from "./hostimpl"
 
 async function main() {
     NodeHost.install()
 
-    const x = parseProject({
-        coarchFiles: ["../sample/test.coarch.md"],
-        promptFiles: [],
-        coarchJsonFiles: [],
-    })
-    // writeFileSync("built/out.json", JSON.stringify(x));
+    const d = `
+    
+ 
+DIFF:
+\`\`\`\`\`diff file=src/pcf8563.ts
+  async readTime(): Promise<Date> {
+-       // TODO
+-       return undefined
++       const buf = await this.readRegBuf(REG_SECONDS, 7)
++       const seconds = this.bcdToDecimal(buf[0] & 0x7f)
++       const minutes = this.bcdToDecimal(buf[1] & 0x7f)
++       const hours = this.bcdToDecimal(buf[2] & 0x3f)
++       const day = this.bcdToDecimal(buf[3] & 0x3f)
++       const month = this.bcdToDecimal(buf[5] & 0x1f)
++       const year = this.bcdToDecimal(buf[6]) + 2000
++       return new Date(year, month - 1, day, hours, minutes, seconds)
+  }
 
-    // tokenize(readFileSync(process.argv[2], "utf8"))
-
-    const r = await getSubtasks({
-        // task: "Create an interactive application, GPUSRUS, that allows users to schedule a cluster of GPU resources. The application has web-based user interface for managing available GPU and reservations by the user. It also has a natural language interface to specify schedule preferences.  For example, a user of GPUSRUS might type into a chat window in the application “I need 4 consecutive hours of an A100 GPU and I prefer evenings but not on weekends” The application, which includes a database of GPUs and an existing schedule, will find open slots in the schedule based on the preferences and schedule them after getting confirmation from the user.",
-        // task: "A simple web app for users to write and save daily journal entries, with options to view past entries and export their journal data.",
-        // task: "A web app that displays a random motivational quote fetched from a built-in list."
-        task: "Compute 2+23.",
-    })
-    console.log(r)
+  async writeTime(date: Date): Promise<void> {
+-       // TODO
+-       return undefined
++       const seconds = this.decimalToBcd(date.seconds)
++       const minutes = this.decimalToBcd(date.minutes)
++       const hours = this.decimalToBcd(date.hours)
++       const day = this.decimalToBcd(date.day)
++       const month = this.decimalToBcd(date.month + 1)
++       const year = this.decimalToBcd(date.year - 2000)
++       await this.writeRegBuf(REG_SECONDS, Buffer.from([seconds, minutes, hours, day, 0x00, month, year]))
+  }
+\`\`\`\`\`    
+            
+    
+    `
+    const { vars } = extractFenced(d)
+    console.log(vars)
 }
 
 main()
