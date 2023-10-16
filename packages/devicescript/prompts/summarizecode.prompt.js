c2dc145b750c7f7a7433f2e8dfb8ec6fac77c208
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-16 23:35:45 +0000

    added fetchtext

--- packages/devicescript/prompts/summarizecode.prompt.js
+++ packages/devicescript/prompts/summarizecode.prompt.js
@@ -3,7 +3,9 @@ prompt({
     description: "Given a source file in a programming language, extract the structure"
 })
 
-defFiles(env.links)
+const { text: content } = await fetchText("https://raw.githubusercontent.com/microsoft/devicescript/main/packages/drivers/src/driver.ts")
+
+def("FILE", { filename: "drivers.ts", content, label: "DeviceScript drivers" })
 
 $`You are an expert at programming in all known languages.
 For each FILE 'filename.extension', generate a summarized FILE 'filename.s.extension' that ignores the internal details
