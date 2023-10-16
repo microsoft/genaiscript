0691621e98e429d5852caa99d8d9d8dc4854e840
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-16 21:37:31 +0000

    fixing date

--- packages/devicescript/src/main.ts
+++ packages/devicescript/src/main.ts
@@ -2,7 +2,7 @@ import { pins, board } from "@dsboard/seeed_xiao_esp32c3"
 import { PCF8563 } from "./pcf8563";
 import { XiaoExpansionBoard } from "@devicescript/drivers";
 import { schedule } from "@devicescript/runtime";
-import { Date } from "@devicescript/core";
+import { Date } from "./date";
 
 const shield = new XiaoExpansionBoard()
 const rtc = new PCF8563();
