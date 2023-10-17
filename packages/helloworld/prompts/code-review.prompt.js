ddfaa4b06993b342ebfbc8d7fa75abe3551257b9
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-17 19:38:39 +0000

    save before auto-run

--- packages/helloworld/prompts/code-review.prompt.js
+++ packages/helloworld/prompts/code-review.prompt.js
@@ -13,8 +13,6 @@ prompt({
 
 def("SPEC", env.file)
 def("CODE", env.links.filter((f) => f.filename.endsWith(".py") && !f.filename.startsWith("test_")))
-if (env.clipboard)
-    def("CLIPBOARD", env.clipboard)
 
 $`
 You are an expert software developer with deep knowledge of the Python programming language.  
@@ -22,8 +20,6 @@ You have been asked to review the code in CODE and provide a code review.
 The code in CODE is intended to solve the problem described in SPEC (ignore the existing Code Review section).  
 The code in CODE is written by a novice programmer.  
 Your job is to critique the code and create a list ways in which it could be improved.`
-if (env.clipboard)
-    $`Analyze CLIPBOARD for runtime errors and suggest code fixes.`
 
 $`Replace the entire "Code Review" section in the SPEC ${env.file.filename} file 
 with your code review. Do not generate python or modify python files. Do not modify file names.
