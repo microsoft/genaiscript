5d66bef8217d2530f03624d48a89899d35d32625
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-23 22:49:31 +0000

    add comments

--- packages/whitepaper/gptools/add-comments.gptool.js
+++ packages/whitepaper/gptools/add-comments.gptool.js
@@ -1,8 +1,9 @@
 gptool({
     title: "Add Comments",
-    description: "Review each chapter and adds a comment as a technical reviewer",
+    description:
+        "Review each chapter and adds a comment as a technical reviewer",
     maxTokens: 4000,
-    system: ["system", "system.diff"]
+    system: ["system", "system.diff"],
 })
 
 const output = env.file.filename + ".comments.md"
@@ -13,9 +14,9 @@ create a list of comments about the document FILE. Address any issues
 around spelling, grammar, clarity, and completeness in FILE. Explain your answers. Only add comments if they required a change in FILE`
 
 $`Generate your comments in markdown using 
-a pseudo diff format as follows:`
+a pseudo diff format as follows:
 
-$`- summary and reason of the change (don't emit "summary:")
+- summary and reason of the change (don't emit "summary:")
 \`\`\`diff
 2 or more lines before the section
 - current lines to change
