cabd628cd44b2a92d5273dea5122c92065003822
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-24 19:01:26 +0000

    updated to task

--- packages/helloworld/gptools/generate-python.gptool.js
+++ packages/helloworld/gptools/generate-python.gptool.js
@@ -2,7 +2,6 @@ gptool({
     title: "Generate python code",
     description: "Given a task, generate python code.",
     temperature: 0,
-    readClipboard: true,
     categories: ["hello world"],
 })
 
@@ -13,12 +12,10 @@ def(
     )
 )
 def("TASK", env.file)
-if (env.clipboard) def("CLIPBOARD", env.clipboard)
 
 $`Generate python code for the task in TASK. Save code in CODE.`
-if (env.clipboard) $`Analyze CLIPBOARD for runtime errors and fix the code.`
 $`If the CODE is already present, ensure that CODE matches the
 description in TASK and make changes to CODE if it does not.
-Do not modify TASK. Do not modify generate tests.`
+Do not modify TASK. Do not generate tests.`
 
 $`Follow the instructions in the Code Review section of TASK to generate CODE.`
