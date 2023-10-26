00d62b7c5cdab0cd88135a5c1ec22304199e7b6a
Ben Zorn <zorn@microsoft.com>
2023-10-26 15:33:25 -0700

    inline file reference

--- packages/core/src/gptools/core.default.gptool.js
+++ packages/core/src/gptools/core.default.gptool.js
@@ -6,8 +6,8 @@ gptool({
 
 def("LINKS", env.links)
 def("TASK", env.file)
-def("BASENAME", env.file.filename)
+
 
 $`The user has defined their task in TASK and provided all the context in LINKS.
 Execute the task as specified in TASK.  If the location of the output is not specified
-append it to file BASENAME.  Do not overwrite the original contents.`
+append it to file ${env.file.filename}.  Do not overwrite the original contents.`
