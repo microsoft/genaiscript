c99afed524a6200eb37e002305ce9cfc621c0346
Ben Zorn <zorn@microsoft.com>
2023-10-26 15:06:35 -0700

    updates to core default gptool

--- packages/core/src/gptools/core.default.gptool.js
+++ packages/core/src/gptools/core.default.gptool.js
@@ -1,11 +1,13 @@
 gptool({
-        title: "Default gptool",
+        title: "Run gpspec directly",
         description: "This is the default gptool that assumes the gpspec contains the entire request.",
         categories: ["core"],
 })
 
 def("LINKS", env.links)
 def("TASK", env.file)
+def("BASENAME", env.file.filename)
 
 $`The user has defined their task in TASK and provided all the context in LINKS.
-Execute the tasks as specified in TASK.`
+Execute the task as specified in TASK.  If the location of the output is not specified
+append it to file BASENAME.  Do not overwrite the original contents.`
