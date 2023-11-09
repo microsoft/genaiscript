d2462ca4f2a217629f756c076f608dfe62604862
Ben Zorn <zorn@microsoft.com>
2023-11-09 15:25:22 -0800

    Removing GAP content from repo

--- packages/whitepaper/gptools/paraphrase-section.gptool.js
+++ packages/whitepaper/gptools/paraphrase-section.gptool.js
@@ -10,9 +10,9 @@ const mainFileName = env.vars.maindoc
 const outFileName = focusFileName?.replace(".md", ".para.md")
 console.log("writing to " + outFileName)
 
-def("FOCUSDOC", env.links.filter(f => f.filename.endsWith(focusFileName)))
-def("MAINDOC", env.links.filter(f => f.filename.endsWith(mainFileName)))
 
+def("MAINDOC", env.links.filter(f => f.filename.endsWith(mainFileName)))
+def("FOCUSDOC", env.links.filter(f => f.filename.endsWith(focusFileName)))
 def("PLAN", env.file)  
 
 // def("OTHERSOURCES", env.links.filter(f => f.filename.endsWith(".md")))
@@ -20,7 +20,10 @@ def("PLAN", env.file)
 $`
 You are an experienced technical writer and you have been given documents describing a project.  The most important files are the PLAN files, that have important instructions for generating the contents of the document.  The PLAN documents contain instructions that should be followed with high priority.
 
-You will paraphrase a section of a larger document that is in MAINDOC.  The section that you will paraphrase is in FOCUSDOC.` 
+When writing, use an active voice.  Instead of saying "are proposed" say "we propose". 
+Don't refer to the original document by calling it "this document". 
 
-$`Paraphrase FOCUSDOC and place the result in ${outFileName} based on MAINDOC and FOCUSDOC. Limit changes to existing text to minimum.`
+You will paraphrase a section of a larger document that is in MAINDOC.  The section that you will paraphrase is in FOCUSDOC.  Structure your write in the following way: (1) Limit the length to 2 pages. (2) Summarize the most important points made in the section first.  (3) After the summary, provide 3 examples from the specific examples given in FOCUSDOC. For each example, write at least four sentences.  (4) Finally summarize possible mitigations mentioned.` 
+
+$`Paraphrase FOCUSDOC and place the result in the file ${outFileName}.`
 
