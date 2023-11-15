fe5274f207a6b337fcc8b2fb7396ca4e4362c66c
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-11-15 17:34:13 +0000

    started on cli

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,10 +1,28 @@
-
-import {
-    parseLLMDiffs,
-} from "coarch-core"
+import { parseLLMDiffs } from "coarch-core"
 import { NodeHost } from "./hostimpl"
+import { program } from "commander"
+
+async function run(
+    str: string,
+    options: {
+        tool: string
+        spec: string
+    }
+) {
+}
 
 async function main() {
+    program.name("gptools").description("CLI for GPTools")
+
+    program
+        .command("run", { isDefault: true })
+        .description("Runs a GPTools against a GPSpec")
+        .requiredOption("-t, --tool <string>", "tool to execute")
+        .requiredOption("-s, --spec", "gpspec file to start from")
+        .action(run)
+
+    program.parse()
+
     NodeHost.install()
     /*
     const source = `
