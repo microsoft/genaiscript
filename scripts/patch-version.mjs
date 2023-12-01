72ac2c4352ddc0497f0394ffc66cc61fd64e1655
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-12-01 20:01:43 +0000

    patch before rebuilding

--- scripts/patch-version.mjs
+++ scripts/patch-version.mjs
@@ -5,20 +5,11 @@ import 'zx/globals'
 const { version } = await fs.readJSON('package.json')
 console.log(`version: ${version}`)
 const packages = await glob(['packages/*/package.json'])
-let added = 0
 for (const pkgp of packages) {
     console.log(`updated ${pkgp}`)
     const pkg = await fs.readJSON(pkgp)
     if (version !== pkg.version) {
-        added++
         pkg.version = version
         await fs.writeJSON(pkgp, pkg, { spaces: 2 })
-        $`git add ${pkgp}`
     }
-}
-
-if (added) {
-    console.log(`git commit versions`)
-    $`git commit -m "chore: bump version to ${version}"`
-    $`git push`
 }
