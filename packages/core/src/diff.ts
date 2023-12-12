4b30902684af2dac6e844ac350b258145a932fa2
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-12-12 21:19:13 +0000

    fix for https://github.com/microsoft/gptools/issues/142

--- packages/core/src/diff.ts
+++ packages/core/src/diff.ts
@@ -12,74 +12,27 @@ export interface Chunk {
  * - some code may be not regenerated
  */
 export function parseLLMDiffs(text: string): Chunk[] {
-    /*
- 
-DIFF src/pcf8563.ts:
-`````diff
-  async readTime(): Promise<Date> {
--       // TODO
--       return undefined
-+       const buf = await this.readRegBuf(REG_SECONDS, 7)
-+       const seconds = this.bcdToDecimal(buf[0] & 0x7f)
-+       const minutes = this.bcdToDecimal(buf[1] & 0x7f)
-+       const hours = this.bcdToDecimal(buf[2] & 0x3f)
-+       const day = this.bcdToDecimal(buf[3] & 0x3f)
-+       const month = this.bcdToDecimal(buf[5] & 0x1f)
-+       const year = this.bcdToDecimal(buf[6]) + 2000
-+       return new Date(year, month - 1, day, hours, minutes, seconds)
-  }
-
-  async writeTime(date: Date): Promise<void> {
--       // TODO
--       return undefined
-+       const buf = this.allocBuffer(7)
-+       buf[0] = this.decimalToBcd(date.seconds) & 0x7f
-+       buf[1] = this.decimalToBcd(date.minutes) & 0x7f
-+       buf[2] = this.decimalToBcd(date.hours) & 0x3f
-+       buf[3] = this.decimalToBcd(date.day) & 0x3f
-+       buf[4] = 0x00 // Weekday not used, set to 0
-+       buf[5] = this.decimalToBcd(date.month + 1) & 0x1f
-+       buf[6] = this.decimalToBcd(date.year - 2000)
-+       await this.writeRegBuf(REG_SECONDS, buf)
-  }
-`````
-
-DIFF src/pcf8563.ts:
-`````diff
-  async stopClock(): Promise<void> {
--       // TODO
-+       const seconds = await this.readReg(REG_SECONDS)
-+       await this.writeReg(REG_SECONDS, seconds | 0x80)
-  }
-`````    
-            
-DIFF ./email_recognizer.py:
-```diff
-[1] import re
-[2] 
-[3] def is_valid_email(email):
-- [4]     if re.fullmatch(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", email):
-+ [4]     pattern = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
-+ [5]     if pattern.fullmatch(email):
-[6]         return True
-[7]     else:
-[8]         return False
-```
-
-*/
     const lines = text.split("\n")
     const chunks: Chunk[] = []
 
     let chunk: Chunk = { state: "existing", lines: [], lineNumbers: [] }
     chunks.push(chunk)
 
+    let currentLine = Number.NaN
     for (let i = 0; i < lines.length; ++i) {
         let line = lines[i]
         const diffM = /^(\[(\d+)\] )?(-|\+) (\[(\d+)\] )?/.exec(line)
         if (diffM) {
             const l = line.substring(diffM[0].length)
-            const diffln = diffM ? parseInt(diffM[5] ?? diffM[2]) : Number.NaN
+            let diffln = diffM ? parseInt(diffM[5] ?? diffM[2]) : Number.NaN
             const op = diffM[3]
+            if (isNaN(diffln) && !isNaN(currentLine)) {
+                currentLine++
+                diffln = currentLine
+                if (op === "-") currentLine--
+            } else {
+                currentLine = diffln
+            }
             if (op === "+") {
                 const l = line.substring(diffM[0].length)
                 if (chunk.state === "added") {
@@ -109,8 +62,14 @@ DIFF ./email_recognizer.py:
             }
         } else {
             const lineM = /^\[(\d+)\] /.exec(line)
-            const lineNumber = lineM ? parseInt(lineM[1]) : Number.NaN
+            let lineNumber = lineM ? parseInt(lineM[1]) : Number.NaN
             const l = line.substring(lineM ? lineM[0].length : 0)
+            if (isNaN(lineNumber) && !isNaN(currentLine)) {
+                currentLine++
+                lineNumber = currentLine
+            } else {
+                currentLine = lineNumber
+            }
             if (chunk.state === "existing") {
                 chunk.lines.push(l)
                 chunk.lineNumbers.push(lineNumber)
