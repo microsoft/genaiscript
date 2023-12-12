a87eb3f5400336f98284fc3323f5d3212d1df16d
pelikhan <jhalleux@microsoft.com>
2023-12-12 10:26:59 -0800

    use node native test runner

--- packages/cli/src/diff.test.ts
+++ packages/cli/src/diff.test.ts
@@ -1,9 +1,9 @@
-import { expect, test } from 'vitest'
-import { parseLLMDiffs } from "./diff"
+import test from "node:test"
+import assert from "node:assert/strict"
+import { parseLLMDiffs } from "gptools-core"
 
 test("is_valid_email", () => {
-    const source = 
-`[1] import re
+    const source = `[1] import re
 [2] 
 [3] def is_valid_email(email):
 - [4]     if re.fullmatch(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", email):
@@ -11,9 +11,7 @@ test("is_valid_email", () => {
 + [5]     if pattern.fullmatch(email):
 [6]         return True
 [7]     else:
-[8]         return False`    
+[8]         return False`
     const chunks = parseLLMDiffs(source)
-    console.log(chunks)
-    expect(chunks.length).toBe(4)
-    expect(chunks).toMatchSnapshot()
+    assert.strictEqual(chunks.length, 4)
 })
