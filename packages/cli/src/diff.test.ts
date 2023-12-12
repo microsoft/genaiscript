20bb8f5e8592ebd24c15223ef0c2195cfedb4d34
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-12-12 21:19:13 +0000

    fix for https://github.com/microsoft/gptools/issues/142

--- packages/cli/src/diff.test.ts
+++ packages/cli/src/diff.test.ts
@@ -15,3 +15,24 @@ test("is_valid_email", () => {
     const chunks = parseLLMDiffs(source)
     assert.equal(chunks.length, 4)
 })
+
+test("missing line numbers", () => {
+    const source = `
+[10] CONSTANT
+-     \* @type: XXX;
++     \* @type: Int;
+[15] VARIABLES
+-     \* @type: [Node -> XXX];
++     \* @type: [Node -> Bool];
+-     \* @type: [Node -> XXX];
++     \* @type: [Node -> Str];
+-     \* @type: XXX;
++     \* @type: Int;
+-     \* @type: XXX;
++     \* @type: Str;
+`
+
+    const chunks = parseLLMDiffs(source)
+    console.log(chunks)
+    assert.equal(chunks.length, 12)
+})
