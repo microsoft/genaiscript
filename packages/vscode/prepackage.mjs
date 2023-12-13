27f6c42311a2f0f97d969461916dadd63169ed7c
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-12-13 17:33:34 +0000

    generate 2 vsix

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -2,6 +2,7 @@ import 'zx/globals'
 
 const pkg = await fs.readJSON('./package.json')
 pkg._enabledApiProposals = pkg.enabledApiProposals
+pkg.displayName = "GPTools"
 delete pkg.enabledApiProposals
 await fs.writeJSON('./package.json', pkg, { spaces: 2 })
 console.log(`cleaned package.json`)
