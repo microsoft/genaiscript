61e3e605f6d478b38d807ec0f6ac4a6a7d2b4ed1
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-03 22:00:38 +0000

    Add function definition for prompt calling

--- packages/core/src/prompt_template.d.ts
+++ packages/core/src/prompt_template.d.ts
@@ -198,6 +198,42 @@ interface ChatAgentContext {
     prompt?: string
 }
 
+interface FunctionDefinition {
+    /**
+     * The name of the function to be called. Must be a-z, A-Z, 0-9, or contain
+     * underscores and dashes, with a maximum length of 64.
+     */
+    name: string
+
+    /**
+     * A description of what the function does, used by the model to choose when and
+     * how to call the function.
+     */
+    description?: string
+
+    /**
+     * The parameters the functions accepts, described as a JSON Schema object. See the
+     * [guide](https://platform.openai.com/docs/guides/text-generation/function-calling)
+     * for examples, and the
+     * [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
+     * documentation about the format.
+     *
+     * Omitting `parameters` defines a function with an empty parameter list.
+     */
+    parameters?: ChatFunctionParameters
+}
+
+/**
+ * The parameters the functions accepts, described as a JSON Schema object. See the
+ * [guide](https://platform.openai.com/docs/guides/text-generation/function-calling)
+ * for examples, and the
+ * [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
+ * documentation about the format.
+ *
+ * Omitting `parameters` defines a function with an empty parameter list.
+ */
+type ChatFunctionParameters = Record<string, unknown>
+
 /**
  * A set of text extracted from the context of the prompt execution
  */
@@ -258,6 +294,14 @@ interface ExpansionVariables {
      * Chat context if called from a chat command
      */
     chat?: ChatAgentContext
+
+    /**
+     * List of functions defined in the prompt
+     */
+    functions?: {
+        definition: FunctionDefinition
+        fn: (args: Record<string, any>) => string | Promise<string>
+    }[]
 }
 
 type MakeOptional<T, P extends keyof T> = Partial<Pick<T, P>> & Omit<T, P>
@@ -280,6 +324,12 @@ interface PromptContext {
     fence(body: StringLike, options?: DefOptions): void
     def(name: string, body: StringLike, options?: DefOptions): void
     defFiles(files: LinkedFile[]): void
+    defFunction(
+        name: string,
+        description: string,
+        parameters: ChatFunctionParameters,
+        fn: (args: Record<string, any>) => string | Promise<string>
+    ): void
     fetchText(urlOrFile: string | LinkedFile): Promise<{
         ok: boolean
         status: number
---
+++ packages/sample/gptools/system.weather.gptool.js
@@ -0,0 +1,23 @@
+system({
+    title: 'Weather function',
+    description: 'Register a function to query the weather'
+})
+
+defFunction('get_current_weather', 'get the current weather', {
+    "type": "object",
+    "properties": {
+        "location": {
+            "type": "string",
+            "description": "The city and state, e.g. San Francisco, CA",
+        },
+        "format": {
+            "type": "string",
+            "enum": ["celsius", "fahrenheit"],
+            "description": "The temperature unit to use. Infer this from the users location.",
+        },
+    },
+    "required": ["location", "format"],
+}, (args) => {
+    const { location, format } = args
+    return `${location} ${format}`
+})
