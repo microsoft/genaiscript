329cbe86b6b3af4788e5101aea0ba9a6226af957
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-04 21:31:10 +0000

    updated spec

--- packages/helloworld/email_recognizer.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def is_valid_email(email):
-    # TODO: use builtin libraries.
-    if re.fullmatch(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", email):
-        return True
-    else:
-        return False
