cfae27cc289107140d6d249ec801f2a4334c0fb0
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-04 22:03:56 +0000

    updated samples

--- packages/helloworld/gptools/generate-python.gptool.js
+++ packages/helloworld/gptools/generate-python.gptool.js
@@ -1,8 +1,6 @@
 gptool({
     title: "Generate python code",
     description: "Given a task, generate python code.",
-    temperature: 0,
-    model: "gpt-4-32k",
     categories: ["hello world"],
 })
 
@@ -17,6 +15,7 @@ def("TASK", env.file)
 $`Generate python code for the task in TASK. Save code in CODE.`
 $`If the CODE is already present, ensure that CODE matches the
 description in TASK and make changes to CODE if it does not.
-Do not modify TASK. Do not generate tests.`
+Do not modify TASK. Do not generate tests.
+`
 
 $`Follow the instructions in the Code Review section of TASK to generate CODE.`
--- packages/helloworld/problem.gpspec.md
+++ packages/helloworld/problem.gpspec.md
@@ -1,4 +1,4 @@
 # email address recognizer
 
-Write a function that validates an email address.
+Write a function that validates an email address and a URL and give me a functiuon that does both.
 -   [email_validator.py](email_validator.py)
