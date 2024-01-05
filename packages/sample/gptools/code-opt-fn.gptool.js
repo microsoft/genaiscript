00df6cb8b6982acfdf92f9511da9121b2f7ff4d7
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-05 17:31:08 +0000

    more logging

--- packages/sample/gptools/code-opt-fn.gptool.js
+++ packages/sample/gptools/code-opt-fn.gptool.js
@@ -4,10 +4,10 @@ gptool({
         "Optimize code to run faster, modified from https://twitter.com/mattshumer_/status/1719403205950349588.",
     maxTokens: 2000,
     categories: ["samples"],
-    system: [],
+    system: ["system.functions"],
 })
 
-defFunction("patch_file", "Describes a patch to a file.", {
+defFunction("update_file", "Describes an update (patch) of a file.", {
     "type": "object",
     "properties": {
         "lineStart": {
@@ -36,16 +36,9 @@ $`
 You are a world expert in making code run faster. You use any resource you can to do so.
 
 Given some code in FILE files, identify how long it might take to run.
-
 After that, identify which parts are key candidates to speed up.
-
 After that, order the candidates by ranking.
 
-Take the top-ranked candidate and explain in more detail how 
-to rewrite the code to be faster using a patch.
-Then, rewrite the actual code. 
-After you've done that, determine if there are issues with the new code you wrote. 
-If so, move on. Otherwise, rewrite the code again to fix them.
-
-Call function patch_file for each speed improvements.
+Take the top-ranked candidate and update the code in the file to be faster.
+Do not explain your reasoning, just update the code.
 `
