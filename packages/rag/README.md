7875dd586de30f943c5b799f9016bde149647418
pelikhan <jhalleux@microsoft.com>
2024-01-17 16:34:08 -0800

    adding a few impl of vector embedding

--- packages/rag/README.md
+++ packages/rag/README.md
@@ -4,13 +4,19 @@ https://docs.trychroma.com/
 
 ## setup
 
+### sqlite3 issue
+
+chromeadb uses a recent version of sqlite and it will typically fail unless [the following steps are taken](https://microsoft.github.io/autogen/docs/FAQ#chromadb-fails-in-codespaces-because-of-old-version-of-sqlite3).
+
+-   install pysqlite3-binary
+
 ```bash
 pip install pysqlite3-binary chromadb
 ```
 
-chromeadb uses a recent version of sqlite and it will typically fail unless [the following steps are taken](https://microsoft.github.io/autogen/docs/FAQ#chromadb-fails-in-codespaces-because-of-old-version-of-sqlite3).
+-   patch paths
 
-```basch
+```bash
 mkdir /home/codespace/.local/lib/python3.10/site-packages/google
 mkdir /home/codespace/.local/lib/python3.10/site-packages/google/colab
 ```
@@ -18,5 +24,5 @@ mkdir /home/codespace/.local/lib/python3.10/site-packages/google/colab
 ## launch server
 
 ```bash
-chroma run --path ./rag
+yarn serve
 ```
--- packages/rag/src/index.ts
+++ packages/rag/src/index.ts
@@ -1,14 +1,66 @@
 import { ChromaClient } from "chromadb"
 import { OpenAIEmbeddingFunction } from "chromadb"
-import { OAIToken } from "gptools-core"
+import { MarkdownTrace, OAIToken } from "gptools-core"
 
-async function startVectorDb(token: OAIToken) {
-    const client = new ChromaClient()
-    const embedder = new OpenAIEmbeddingFunction({
-        openai_api_key: token.token,
+export interface VectorToken {
+    token?: string
+    embeddingToken: OAIToken
+}
+
+async function startVectorDb(token: VectorToken) {
+    if (!token.embeddingToken.isOpenAI) throw new Error("Invalid OpenAI token")
+
+    const client = new ChromaClient({
+        auth: token.token
+            ? { provider: "token", credentials: "test-token" }
+            : undefined,
     })
-    const collection = await client.createCollection({
+    const embedder = new OpenAIEmbeddingFunction({
+        openai_api_key: token.embeddingToken.token,
+    })
+    const collection = await client.getOrCreateCollection({
         name: "sources",
         embeddingFunction: embedder,
     })
+    return { client, collection }
+}
+
+export async function stats(token: VectorToken, trace: MarkdownTrace) {
+    try {
+        trace.startDetails(`embedded documents`)
+        const { collection } = await startVectorDb(token)
+
+        const count = await collection.count()
+        trace.item(`count: ${count}`)
+    } catch (e) {
+        trace.error("error", e)
+    } finally {
+        trace.endDetails()
+    }
+    return stats
+}
+
+export async function upsertFiles(token: VectorToken, files: LinkedFile[]) {
+    const { collection } = await startVectorDb(token)
+    return await collection.upsert({
+        ids: files.map((file) => file.filename),
+        documents: files.map((file) => file.content),
+        metadatas: files.map((file) => ({
+            label: file.label,
+            filename: file.filename,
+        })),
+    })
+}
+
+export async function query(
+    token: VectorToken,
+    queryTexts: string
+): Promise<string[]> {
+    const { collection } = await startVectorDb(token)
+    const query = await collection.query({
+        nResults: 10,
+        queryTexts,
+    })
+    const documents = query.documents?.[0]
+    return documents.filter((d) => d !== null) as string[]
 }
