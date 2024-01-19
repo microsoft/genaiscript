5bc45d9dddc9018804bb5a96fe26676b208812ba
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-19 22:02:31 +0000

    stream json to files

--- applications/lza/gptools/lza_review.gptool.js
+++ applications/lza/gptools/lza_review.gptool.js
@@ -1,9 +1,9 @@
 gptool({
-    title: "LZA review",
-    description: "Analyze the contents of SPEC files from a git repository of a Microsoft Azure Landing Zone Accelerator (LZA) for an enterprise software company. The user will share a file and its dependencies for you to analyze.",
-    categories: ["Azure Landing Zone"],
-    system: ["system", "system.explanations", "system.technical", "system.annotations"],
-    model: "gpt-4"
+  title: "LZA review",
+  description: "Analyze the contents of SPEC files from a git repository of a Microsoft Azure Landing Zone Accelerator (LZA) for an enterprise software company. The user will share a file and its dependencies for you to analyze.",
+  categories: ["Azure Landing Zone"],
+  system: ["system", "system.explanations", "system.files", "system.technical", "system.annotations"],
+  model: "gpt-4"
 })
 
 // use $ to output formatted text to the prompt
@@ -49,34 +49,46 @@ Use ANNOTATIONS to highlight the issues, best practices or improvements in SPECS
 4. Double check your list of actions to make sure that you have a complete list of actions and add any that you missed
 5. Organize the actions by the target services or cloud components that are impacted by the actions
 6. Identify any cloud resources that are deployed after this file is used that were not present before this file was used and add them to the "Deployed" list. If nothing that you find directly within the provided file content meets this criteria, please leave the list empty.
-7. Generate a YAML file with the results formatted as follow:
+7. Generate a JSON file, targets.json, with the results following the JSON schema below:
 `
 
-fence(`targets:
-  target:
-    name: <target name>
-    source: <path to the file>
-    action: <description of the action>
-deployments:
-  deployment:
-    name: <deployment name>
-    source: <path to the file>
-`, { language: "yaml" })
+fence(JSON.stringify({
+  type: "array",
+  description: "An array of targets",
+  items: {
+    description: "A target that is impacted by the actions in the file",
+    type: "object",
+    properties: {
+      "name": {
+        "description": "Identifier of the target",
+        "type": "string"
+      },
+      "source": {
+        "description": "Path of the file defining the target",
+        "type": "string"
+      },
+      "action": {
+        "description": "What is being done on the cloud resource",
+        "type": "string"
+      }
+    }
+  }
+}))
 
 const biceps = env.links.filter(f => f.filename.endsWith(".bicep"))
 def("SPECS", biceps, { lineNumbers: true })
 
 // inline dependencies
 for (const link of biceps) {
-    const filename = link.filename
-    const dirname = filename.split(/\//g).slice(0, -1).join("/") + "/"
-    const content = link.content
-    const dependencies = content.matchAll(/module\s+([^\s]+)\s+\'([^']+)'/g)
-    for (const dependency of dependencies) {
-        const [, , path] = dependency
-        if (path.includes("shared")) continue // ignore those shared files
-        const dp = dirname + path
-        const resp = await fetchText(dp)
-        def("DEPS", resp.file, { lineNumbers: true })
-    }
+  const filename = link.filename
+  const dirname = filename.split(/\//g).slice(0, -1).join("/") + "/"
+  const content = link.content
+  const dependencies = content.matchAll(/module\s+([^\s]+)\s+\'([^']+)'/g)
+  for (const dependency of dependencies) {
+    const [, , path] = dependency
+    if (path.includes("shared")) continue // ignore those shared files
+    const dp = dirname + path
+    const resp = await fetchText(dp)
+    def("DEPS", resp.file, { lineNumbers: true })
+  }
 }
