c98002b1aff56f5a7719c2b001df373a2e2a2360
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-19 07:53:51 +0000

    restrict analysis per file

--- applications/lza/gptools/lza_review.gptool.js
+++ applications/lza/gptools/lza_review.gptool.js
@@ -10,16 +10,21 @@ gptool({
 $`# Context
 
 You are a Azure Bicep language expert.
-You will analyze the contents of SPEC files from a git repository of a Microsoft Azure Landing Zone Accelerator (LZA) for an enterprise software company.
+You will analyze the contents of SPEC files 
+from a git repository of a Microsoft Azure Landing Zone Accelerator (LZA) 
+for an enterprise software company.
 
 ## Objective
 
-Identify security issues in SPEC files. Use ANNOTATIONS to highlight the issues, best practices or improvements. Do NOT generate a bullet point list.
+Identify security issues in SPEC files. 
+Use ANNOTATIONS to highlight the issues, best practices or improvements in SPECS. 
 
 -  Look for bad practices.
 -  Look for weak secrets and passwords
 -  Look for any pattern that would to a security issue.
 -  Add link to documentation about security issues.
+-  Do NOT generate annotations for DEPS files.
+-  Do NOT generate a bullet point list. 
 `
 
 const biceps = env.links.filter(f => f.filename.endsWith(".bicep"))
@@ -36,6 +41,6 @@ for (const link of biceps) {
         if (path.includes("shared")) continue // ignore those shared files
         const dp = dirname + path
         const resp = await fetchText(dp)
-        def("SPECS", resp.file, { lineNumbers: true })
+        def("DEPS", resp.file, { lineNumbers: true })
     }
 }
