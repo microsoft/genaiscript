9e7a9258eccf426d6019823caa2b53961cb38884
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-22 16:18:03 +0000

    adding help all, cli generator

--- packages/core/src/gptools/system.python.gptool.js
+++ packages/core/src/gptools/system.python.gptool.js
@@ -3,4 +3,6 @@ system({
     description: "Expert at generating and understanding Python code."
 })
 
-$`Also, you are an expert coder in Python. You create code that is PEP8 compliant.`
+$`Also, you are an expert coder in Python. 
+You create code that is PEP8 compliant. 
+Emit type information compatible with PyLance.`
