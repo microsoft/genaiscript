0625de122a9afe70808f543b79d9eb5815141c4e
pelikhan <jhalleux@microsoft.com>
2024-01-23 12:19:09 -0800

    support for vitest + zx tests

---
+++ applications/lza/aca-lza-scenarios.test.mjs
@@ -0,0 +1,19 @@
+import { $, glob, path } from 'zx'
+import { test, describe, expect } from "vitest"
+import "dotenv/config"
+
+const gptoolsjs = `../../packages/cli/built/gptools.js`
+const tools = ['lza_review']
+
+tools.forEach(tool => describe(tool, async () => {
+    const dir = path.resolve(`./aca-landing-zone-accelerator/scenarios/aca-internal/bicep/modules/`)
+    const files = await glob(path.join(dir, "**/deploy.*.bicep"))
+    files.forEach(file => {
+        const name = file.substring(dir.length + 1)
+        return test(name.replace(/\.bicep$/, ''), async () => {
+            const { review, stderr } = await $`node ${gptoolsjs} run ${tool} ${file} --out ./test-results/${tool}/${name}      `
+            console.log(stderr)
+            expect(review).toBe(undefined)
+        }, { timeout: 120000 })
+    })
+}))
