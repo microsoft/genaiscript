645bf265ef66cb8ec0a24408fbe7239f6526f43a
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-24 15:48:58 +0000

    added toml parser

--- packages/core/src/parsers.ts
+++ packages/core/src/parsers.ts
@@ -1,9 +1,11 @@
 import { JSON5TryParse } from "./json5"
+import { TOMLTryParse } from "./toml"
 import { YAMLTryParse } from "./yaml"
 
 export function createParsers(): Parsers {
     return {
         JSON5: (text) => JSON5TryParse(text),
         YAML: (text) => YAMLTryParse(text),
+        TOML: (text) => TOMLTryParse(text),
     }
 }
