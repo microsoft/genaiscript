287cf5f6c0925702823738d99293e90b979ece7a
pelikhan <jhalleux@microsoft.com>
2024-01-25 15:56:04 -0800

    updated samples

--- packages/core/src/gptools/system.schema.gptool.js
+++ packages/core/src/gptools/system.schema.gptool.js
@@ -7,6 +7,7 @@ $`## JSON Schema
 A JSON schema is a named JSON object that defines the structure of a JSON object. 
 The schema is used to validate JSON objects and to generate JSON objects. 
 It is stored in a \`json-schema\` code section.
+JSON schemas can also be applied to YAML or TOML files.
 
     <schema-identifier>:
     \`\`\`json-schema
