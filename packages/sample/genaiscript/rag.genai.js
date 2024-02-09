e7257d75478ecf3c5fcfb0392f9b7f687a18807d
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-09 21:21:47 +0000

    update rag scenario

--- packages/sample/genaiscript/rag.genai.js
+++ packages/sample/genaiscript/rag.genai.js
@@ -5,6 +5,6 @@ script({
 // use $ to output formatted text to the prompt
 $`You are a helpful assistant. Summarize the files.`
 
-const res = await retreive("email", env.files)
+const res = await retreive("lorem ipsum", env.files)
 // use def to emit LLM variables
 def("FILE", res)
