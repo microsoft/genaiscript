fdc40cc9fa73b4782330265397c4befa249e084b
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-10 19:14:43 +0000

    removing older samples: demoapps, mywordle, makecode, devicescript, mystory

--- packages/mywordle/bugnotes.md
+++
@@ -1,8 +0,0 @@
-# Notes on bugs in the code
-
-## Past issues
-## New issues
-
-- Use the file "dictionary.txt" for the 5-letter word list.
-- Print out the number of turns left after each guess.
-
--- packages/mywordle/gamedemo/game_logic.py
+++
@@ -1,6 +0,0 @@
-from typing import List, Tuple
-
-def compare_words(user_word: str, hidden_word: str) -> Tuple[List[Tuple[int, str]], List[str]]:
-    correct_positions = [(i, user_word[i]) for i in range(5) if user_word[i] == hidden_word[i]]
-    correct_letters = [user_word[i] for i in range(5) if user_word[i] in hidden_word and (i, user_word[i]) not in correct_positions]
-    return correct_positions, correct_letters
