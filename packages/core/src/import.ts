9ef85f37bb5154ec953e5191de441e48ba8aa6bb
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-13 17:18:54 +0000

    refactor importer

---
+++ packages/core/src/import.ts
@@ -0,0 +1,23 @@
+import { exec } from "./exec"
+import { host } from "./host"
+import { MarkdownTrace } from "./trace"
+import { fileExists } from "./util"
+
+export async function installImport(trace: MarkdownTrace, id: string) {
+    const cwd = host.installFolder()
+    const yarn = await fileExists(host.path.join(cwd, "yarn.lock"))
+    const command = yarn ? "yarn" : "npm"
+    const args = yarn
+        ? ["add", id]
+        : ["install", "--no-save", "--ignore-scripts", id]
+    const res = await exec(host, trace, {
+        label: `install ${id}`,
+        call: {
+            type: "shell",
+            command,
+            args,
+            cwd,
+        },
+    })
+    return res.exitCode === 0
+}
