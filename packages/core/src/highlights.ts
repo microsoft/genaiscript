9786f1b9266fae0e817cb7d83d2f5a983bb9e008
pelikhan <jhalleux@microsoft.com>
2024-02-17 17:45:29 -0500

    filter supported files

--- packages/core/src/highlights.ts
+++ packages/core/src/highlights.ts
@@ -1,16 +1,37 @@
-import { host } from "genaiscript-core"
+import { highlightsLanguages, host } from "genaiscript-core"
+
+function isSupported(f: string) {
+    const ext = host.path.extname(f).slice(1).toLowerCase()
+    const map: Record<string, string> = {
+        js: "javascript",
+        cs: "c_sharp",
+        sh: "bash",
+        py: "python",
+        rs: "rust",
+        ts: "typescript",
+    }
+    return ext && highlightsLanguages.includes(map[ext] || ext)
+}
+
+/*
+export const highlightsLanguages: string[] = ["bash","c","c_sharp","cpp","css","elisp","elixir","elm","embedded_template","go","html","java","javascript","json","lua","ocaml","php","python","ql","rescript","ruby","rust","systemrdl","toml","tsx","typescript","vue","yaml"];
+*/
 
 export async function highlight(
     files: LinkedFile[],
     options: HighlightOptions
 ) {
-    const codeFiles = files.filter(({ content }) => !!content)
+    const codeFiles = files.filter(
+        ({ filename, content }) => !!content && isSupported(filename)
+    )
     const service = host.highlight
     return await service.highlight(codeFiles, options)
 }
 
 export async function outline(files: LinkedFile[]) {
-    const codeFiles = files.filter(({ content }) => !!content)
+    const codeFiles = files.filter(
+        ({ filename, content }) => !!content && isSupported(filename)
+    )
     const service = host.highlight
     return await service.outline(codeFiles)
 }
