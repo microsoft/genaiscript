4ce2e344542ea1ac6f26f10f3fcfe5b0cddba9dc
pelikhan <jhalleux@microsoft.com>
2024-02-19 17:33:04 -0500

    updated lock

--- packages/core/src/tokens.ts
+++ packages/core/src/tokens.ts
@@ -1,12 +1,18 @@
 import { ChatCompletionRequestMessage } from "./chat"
 import type { ChatCompletionTool } from "openai/resources"
-import { promptTokensEstimate } from "openai-chat-tokens"
+import { encodeChat } from "gpt-tokenizer"
 
 export function estimateTokens(
     model: string,
     messages: ChatCompletionRequestMessage[],
     tools: ChatCompletionTool[]
 ): number {
-    const res = promptTokensEstimate({ messages })
-    return res
+    const chat: { role: "user" | "system" | "assistant"; content: string }[] =
+        messages.filter(
+            ({ role }) =>
+                role === "user" || role === "system" || role === "assistant"
+        ) as any
+    const chatTokens = encodeChat(chat, model as any)
+
+    return chatTokens.length
 }
