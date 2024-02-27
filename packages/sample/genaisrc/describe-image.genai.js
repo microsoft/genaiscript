fd6b0cd9135454e0d1d3b7ec1056199307665b9d
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-27 05:11:39 +0000

    smaller image

--- packages/sample/genaisrc/describe-image.genai.js
+++ packages/sample/genaisrc/describe-image.genai.js
@@ -1,8 +1,8 @@
 script({
-    title: "describe-image",
+    title: "Describe objects in image",
     model: "gpt-4-turbo-v",
     system: [],
 })
 
 $`Return the list of objects in the images.`
-defImages(env.files, {detail:"low"})
+defImages(env.files, { detail: "low" })
