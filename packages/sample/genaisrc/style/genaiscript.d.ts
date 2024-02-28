f71ae09aa34505d6698108de0555b1e406787869
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-28 00:48:47 +0000

    add filter for file extensions

--- packages/sample/genaisrc/style/genaiscript.d.ts
+++ packages/sample/genaisrc/style/genaiscript.d.ts
@@ -443,6 +443,16 @@ interface DefOptions {
      * JSON schema identifier
      */
     schema?: string
+
+    /**
+     * Filename filter based on file suffix. Case insensitive.
+     */
+    endsWith?: string
+
+    /**
+     * Filename filter using glob syntax.
+     */
+    glob?: string
 }
 
 interface DefImagesOptions {
@@ -625,7 +635,6 @@ interface PromptContext {
     system(options: PromptArgs): void
     fence(body: StringLike, options?: DefOptions): void
     def(name: string, body: StringLike, options?: DefOptions): void
-    defFiles(files: LinkedFile[]): void
     defImages(files: StringLike, options?: DefImagesOptions): void
     defFunction(
         name: string,
@@ -696,14 +705,6 @@ declare function fence(body: StringLike, options?: DefOptions): void
  * @param body string to be fenced/defined
  */
 declare function def(name: string, body: StringLike, options?: DefOptions): void
-
-/**
- * Inline supplied files in the prompt.
- * Similar to `for (const f in files) { def("File " + f.filename, f.contents) }`
- *
- * @param files files to define, eg. `env.files` or a subset thereof
- */
-declare function defFiles(files: LinkedFile[]): void
 
 /**
  * Declares a function that can be called from the prompt.
