1041eee41f903518f5b2a8c2bc0300cbf6d6c088
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-02-28 20:33:41 +0000

    fix token compute

--- packages/core/src/tokens.ts
+++ packages/core/src/tokens.ts
@@ -1,5 +1,8 @@
 import { ChatCompletionRequestMessage } from "./chat"
-import type { ChatCompletionTool } from "openai/resources"
+import {
+    ChatCompletionContentPartText,
+    type ChatCompletionTool,
+} from "openai/resources"
 import { encodeChat, encode } from "gpt-tokenizer"
 import { logError } from "./util"
 
@@ -19,13 +22,27 @@ export function estimateChatTokens(
     tools?: ChatCompletionTool[]
 ): number {
     const chat: { role: "user" | "system" | "assistant"; content: string }[] =
-        messages.filter(
-            ({ role }) =>
-                role === "user" || role === "system" || role === "assistant"
-        ) as any
+        messages
+            .filter(
+                ({ role }) =>
+                    role === "user" || role === "system" || role === "assistant"
+            )
+            .map(({ role, content }) => ({
+                role: role as "user" | "system" | "assistant",
+                content:
+                    typeof content === "string"
+                        ? content
+                        : content
+                              .filter(({ type }) => type === "text")
+                              .map(
+                                  (c) =>
+                                      (c as ChatCompletionContentPartText).text
+                              )
+                              .join("\n"),
+            }))
     try {
         const chatTokens = encodeChat(chat, model as any)
-        return (chatTokens.length) | 0
+        return chatTokens.length | 0
     } catch (e) {
         logError(e.message)
         return JSON.stringify(chat).length >> 2
