99565d4dffd48129acdc06848b889160d9e1122d
Ben Zorn <zorn@microsoft.com>
2024-03-07 21:47:29 +0000

    Saving preliminary doc generating script

---
+++ packages/sample/genaisrc/generate-documentation-feature-guide.genai.js
@@ -0,0 +1,22 @@
+// metadata (including model parameters)
+script({ title: "generate-doc-feature-guide",
+         categories: ["doc tools"], 
+})
+
+// use def to emit LLM variables
+def("EXAMPLEGUIDES", env.files, { endsWith: "guide.md" })
+def("EXAMPLEFEATURES", env.files, { endsWith: "feature.md" })
+// def("NEWFEATURE", env.files, { endsWith: "new.md" })
+
+def("NEWFEATURE", env.files, { endsWith: "new.md" })
+
+// use $ to output formatted text to the prompt
+$`You are a technical writer and software documentation expert.
+You are given examples of documentation for different 
+system features in EXAMPLES.  There is a new feature 
+in the same system documented in NEWFEATURE.  Write a
+user guide similar to those in EXAMPLES for the new feature.
+Make sure the example illustrates the feature with minimal
+extra complexity.
+Write the output in .mdx format.
+`        
