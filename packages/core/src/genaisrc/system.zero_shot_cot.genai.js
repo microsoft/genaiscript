ce00bdce5a5890c8928474ca6f144cc4f4e0f48e
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-08 18:05:29 +0000

    added zscot

---
+++ packages/core/src/genaisrc/system.zero_shot_cot.genai.js
@@ -0,0 +1,6 @@
+script({
+    title: "Zero-shot Chain of Thought",
+    description:
+        "Zero-shot Chain Of Though technique. More at https://learnprompting.org/docs/intermediate/zero_shot_cot.",
+})
+$`Let's think step by step.`
