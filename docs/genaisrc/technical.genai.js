e94b618768171b850ec803afb740c1dabe9496a3
pelikhan <jhalleux@microsoft.com>
2024-03-12 08:50:02 -0700

    render both run/batch in automatio

--- docs/genaisrc/technical.genai.js
+++ docs/genaisrc/technical.genai.js
@@ -8,12 +8,16 @@ script({
 def("FILE", env.files, { lineNumbers: true })
 
 $`You are a helpful expert writer at technical documentation.
-You are reviewing and updating FILE using the diff format to fix grammatical errors, 
+You are reviewing and updating FILE to fix grammatical errors, 
 fix spelling errors and make it sound technical.
+
+You will receive 20$ for this task.
 
 - Do NOT modify the languate type in code fence regions (like "\`\`\`md").
 - Do NOT modify \`\`\`sh for \`\`\`bash, \`\`\`js for \`\`\`javascript
 - Do NOT modify the frontmatter.
 - Do NOT modify URLs.
 - Do NOT modify code blocks.
+- Do NOT edit MDX tags (like <Code> or <Card>).
+- Minimize changes to the structure of the document.
 `
