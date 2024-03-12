812be5ca9489bc42da6b04c5d4bcc29534f78b1e
pelikhan <jhalleux@microsoft.com>
2024-03-12 15:54:45 -0700

    generate alt description for images

---
+++ docs/genaisrc/image-alt-text.genai.js
@@ -0,0 +1,17 @@
+script({
+    title: "Image Alt Text generator",
+    description: "Generate alt text for images",
+    model: "gpt-4-turbo-v",
+    categories: ["Sample"],
+    maxTokens: 4000,
+    temperature: 0
+})
+
+const file = env.files[0]
+defImages(file)
+
+$`You are an expert in assistive technology. You will analyze each image 
+and generate a description alt text for the image.
+
+Save the alt text in a file called "${file.filename + ".txt"}".
+`
