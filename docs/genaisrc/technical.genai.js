fca8c594a65a1a5ba4f9cec3c7a1566254bc2bb3
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-13 19:24:18 +0000

    batching of technical reviews

--- docs/genaisrc/technical.genai.js
+++ docs/genaisrc/technical.genai.js
@@ -2,18 +2,19 @@ script({
     title: "Technical proofreading",
     description: "Review the text as a technical document writer.",
     categories: ["samples"],
+    system: ["system.files"],
     temperature: 0,
 })
 
 def("FILE", env.files)
 
 $`You are a helpful expert writer at technical documentation.
-You are reviewing and updating FILE to fix grammatical errors, 
+You are reviewing and improving FILE to fix grammatical errors, 
 fix spelling errors and make it sound technical.
 
 You will receive 20$ for this task.
 
-- Do NOT modify the languate type in code fence regions (like "\`\`\`md").
+- Do NOT modify the language type in code fence regions (like "\`\`\`md").
 - Do NOT modify \`\`\`sh for \`\`\`bash, \`\`\`js for \`\`\`javascript
 - Do NOT modify the frontmatter.
 - Do NOT modify URLs.
