8798b4c6f57b5294b76de887c8bcdd9459c3d375
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-14 03:58:28 +0000

    don't show system prompts in selector

--- packages/core/src/genaisrc/system.zero_shot_cot.genai.js
+++ packages/core/src/genaisrc/system.zero_shot_cot.genai.js
@@ -1,4 +1,4 @@
-script({
+system({
     title: "Zero-shot Chain of Thought",
     description:
         "Zero-shot Chain Of Though technique. More at https://learnprompting.org/docs/intermediate/zero_shot_cot.",
