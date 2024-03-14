e0ad8bc9c080dd177d3753bc3b6cc37b4b322ad3
pelikhan <jhalleux@microsoft.com>
2024-03-13 20:16:20 -0700

    generate makecode translation script

--- packages/sample/src/makecode/makecode-loc.genai.js
+++ packages/sample/src/makecode/makecode-loc.genai.js
@@ -1,6 +1,6 @@
 script({
-  title: "MakeCode Blocks Localization",
-  description: "Translation block strings for MakeCode",
+    title: "MakeCode Blocks Localization",
+    description: "Translation block strings for MakeCode",
 })
 
 /**
@@ -23,42 +23,62 @@ script({
 const langCode = env.vars.lang || "fr"
 // given a language code, refer to the full name to help the LLM
 const langName = {
-  "fr": "French",
-  "es-ES": "Spanish",
-  "de": "German",
-  "sr": "Serbian",
-  "vi": "Vietnamese",
-  "it": "Italian"
+    fr: "French",
+    "es-ES": "Spanish",
+    de: "German",
+    sr: "Serbian",
+    vi: "Vietnamese",
+    it: "Italian",
 }[langCode]
-if (!langName)
-  cancel('unknown language')
+if (!langName) cancel("unknown language")
 
-// grab the *-strings.json file, filter out the jsdoc files
+// find the first compatible file
+const file = env.files.find(
+    ({ filename }) =>
+        filename.endsWith("-strings.json") &&
+        !filename.includes("jsdoc") &&
+        !/_locales\/\w+\/[/]+-strings\.json/.test(filename)
+)
+if (!file) cancel("no strings file found")
+
 // convert JSON to YAML as it typically works better with LLM tokenizers
-const files = env.files.filter(({ filename }) => filename.endsWith("-strings.json") && !filename.includes("jsdoc"))
-for (const { filename, label, content } of files) {
-  const strings = JSON.parse(content)
-  // find the existing translation and remove existing translations
-  const trfn = path.join(path.dirname(filename), langCode, path.basename(filename))
-  const translated = parsers.JSON5(await fs.readFile(trfn))
-  if (translated) {
+const { filename, label, content } = file
+const strings = JSON.parse(content)
+// find the existing translation and remove existing translations
+const trfn = path.join(
+    path.dirname(filename),
+    langCode,
+    path.basename(filename)
+)
+const translated = parsers.JSON5(await fs.readFile(trfn))
+if (translated) {
     for (const k of Object.keys(strings)) {
-      if (translated[k]) {
-        delete strings[k]
-      }
+        if (translated[k]) {
+            delete strings[k]
+        }
     }
-  }
-
-  // use YAML for content as it work better with LLMs
-  const contentToTranslate = YAML.stringify(strings).replace(/\* \`\`\`/g, "  ```")
-
-  // add to prompt context
-  def("ORIGINAL", {
-    filename,
-    label,
-    content: contentToTranslate,
-  }, { language: "yaml" })
 }
+
+// shortcut: all translation is done
+if (Object.keys(strings).length === 0) cancel(`no strings to translate`)
+
+// use YAML for content as it work better with LLMs
+// fix some makecode formatting issues
+const contentToTranslate = YAML.stringify(strings).replace(
+    /\n\* \`\`\`/g,
+    "\n  ```"
+)
+
+// add to prompt context
+def(
+    "ORIGINAL",
+    {
+        filename,
+        label,
+        content: contentToTranslate,
+    },
+    { language: "yaml" }
+)
 
 // the prompt engineering piece
 $`
@@ -72,14 +92,14 @@ You are an expert ${langName} translator.
 ## Task
 
 Translate the documentation in ORIGINAL to ${langName} (lang-iso '${langCode}').
-If the file is '<path>/foobar-strings.json', save the translation as '<path>/${langCode}/foobar-strings.json' in JSON.
+Write the translation to file "${trfn}" in JSON.
 
 The ORIGINAL files are formatted in YAML where the key is an identifer and the value is the localized string.
 
-EXAMPLE:
+EXAMPLE of ORIGINAL:
 \`\`\`yaml
-key1: "localized value 1"
-key2: "localized value 2"
+key1: localized value 1
+key2: localized value 2
 \`\`\`
 
 ## Recommendations
@@ -95,18 +115,16 @@ The value for keys ending with "|block" are MakeCode block strings (https://make
 - Do NOT translate variable names.
 - Some variable names have a value, like '%foo=toggleOnOff'. The value should be NOT translated.
 - All variables in the original string should be in the translated string.
+- Make sure to translate '\\%' to '\\%' and '\\$' to '\\$' if they are not variables.
 `
 
 // merge the translations with the old one and marshal yaml to json
-defFileMerge((filename,
-  label,
-  before,
-  generated) => {
-  if (!filename.endsWith("-strings.json")) return undefined
-  const olds = JSON.parse(before || "{}")
-  const news = JSON.parse(generated)
-  // keep old translations
-  Object.assign(news, olds)
-  // return stringified json
-  return JSON.stringify(olds, null, 2)
+defFileMerge((filename, label, before, generated) => {
+    if (!filename.endsWith("-strings.json")) return undefined
+    const olds = JSON.parse(before || "{}")
+    const news = JSON.parse(generated)
+    // keep old translations
+    Object.assign(olds, news)
+    // return stringified json
+    return JSON.stringify(olds, null, 2)
 })
