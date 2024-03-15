61b806adf711d25d2c4647759cb909223193eb1c
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-15 21:02:26 +0000

    attempt at fixing pages (somehow)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from "@astrojs/starlight"
 // https://astro.build/config
 export default defineConfig({
     site: "https://microsoft.github.io",
-    base: "/genaiscript",
+    // no base in private github pages
+    // base: "/genaiscript",
     integrations: [
         starlight({
             title: "GenAIScript",
