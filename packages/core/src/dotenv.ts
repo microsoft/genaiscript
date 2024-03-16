bab25505a3f1d3a4abe8df87280f42eaf945fde7
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-16 16:33:26 +0000

    add more .env support

--- packages/core/src/dotenv.ts
+++ packages/core/src/dotenv.ts
@@ -11,3 +11,17 @@ export function dotEnvTryParse(text: string): Record<string, string> {
 }
 
 export const dotEnvParse = parse
+
+export function dotEnvStringify(record: Record<string, string>): string {
+    return Object.entries(record || {})
+        .map(([key, value]) => {
+            if (value === undefined || value === null) value = ""
+            // If the value contains newlines or quotes, enclose it in double quotes and escape existing quotes
+            if (value.includes("\n") || value.includes('"')) {
+                value = value.replace(/"/g, '\\"') // Escape existing quotes
+                return `${key}="${value}"`
+            }
+            return `${key}=${value}`
+        })
+        .join("\n")
+}
