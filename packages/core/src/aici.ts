9eaaf5dcf5f074416000354364c1d1dc51c223ac
pelikhan <jhalleux@microsoft.com>
2024-03-19 23:28:57 -0700

    don't trace aici

--- packages/core/src/aici.ts
+++ packages/core/src/aici.ts
@@ -36,71 +36,60 @@ export interface AICIRequest {
 
 export async function renderAICI(
     functionName: string,
-    root: PromptNode,
-    options?: { trace: MarkdownTrace }
+    root: PromptNode
 ): Promise<AICIRequest> {
-    const { trace } = options
     const notSupported = (reason: string) => (n: any) => {
         throw new NotSupportedError(reason)
     }
 
-    try {
-        trace?.startDetails("aici")
-        trace?.itemValue("controller", "jsctrl")
-        let program: string[] = []
-        let indent: string = ""
-        const push = (text: string) => program.push(indent + text)
+    let program: string[] = []
+    let indent: string = ""
+    const push = (text: string) => program.push(indent + text)
 
-        push(`async function ${functionName}() {`)
-        indent = "  "
-        await visitNode(root, {
-            text: async (n) => {
-                const value = await n.value
-                if (value !== undefined)
-                    // TODO escape javascript string to `...`
-                    push(`await fixed(\`${escapeJavascriptString(value)}\``)
-            },
-            stringTemplate: async (n) => {
-                const { strings, args } = n
-                let r = "await $`"
-                for (let i = 0; i < strings.length; ++i) {
-                    r += escapeJavascriptString(strings[i])
-                    if (i < args.length) {
-                        const arg = await args[i]
-                        if (typeof arg === "string") {
-                            r += escapeJavascriptString(arg)
-                        } else if (arg.type === "aici") {
-                            const rarg = renderAICINode(arg)
-                            r += "${" + rarg + "}"
-                        } else {
-                            const rarg = JSON.stringify(arg)
-                            r += "${" + rarg + "}"
-                        }
+    push(`async function ${functionName}() {`)
+    indent = "  "
+    await visitNode(root, {
+        text: async (n) => {
+            const value = await n.value
+            if (value !== undefined)
+                // TODO escape javascript string to `...`
+                push(`await fixed(\`${escapeJavascriptString(value)}\``)
+        },
+        stringTemplate: async (n) => {
+            const { strings, args } = n
+            let r = "await $`"
+            for (let i = 0; i < strings.length; ++i) {
+                r += escapeJavascriptString(strings[i])
+                if (i < args.length) {
+                    const arg = await args[i]
+                    if (typeof arg === "string") {
+                        r += escapeJavascriptString(arg)
+                    } else if (arg.type === "aici") {
+                        const rarg = renderAICINode(arg)
+                        r += "${" + rarg + "}"
+                    } else {
+                        const rarg = JSON.stringify(arg)
+                        r += "${" + rarg + "}"
                     }
                 }
-                r += "`"
-                push(r)
-            },
-            image: notSupported("image"),
-            function: notSupported("function"),
-            // TODO?
-            schema: notSupported("schema"),
-            // ignore
-            // outputProcessor,
-            // fileMerge,
-        })
+            }
+            r += "`"
+            push(r)
+        },
+        image: notSupported("image"),
+        function: notSupported("function"),
+        // TODO?
+        schema: notSupported("schema"),
+        // ignore
+        // outputProcessor,
+        // fileMerge,
+    })
 
-        indent = ""
-        push("}")
+    indent = ""
+    push("}")
 
-        const content = program.join("\n")
-        return { role: "aici", content, functionName }
-    } catch (error) {
-        trace?.error("AICI code generation error", error)
-        throw error
-    } finally {
-        trace?.endDetails()
-    }
+    const content = program.join("\n")
+    return { role: "aici", content, functionName }
 }
 
 interface ModelInitialRun {
