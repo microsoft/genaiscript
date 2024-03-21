863d15adb5abf01ee46163c4a2d9400f9688ee00
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-21 05:02:44 +0000

    add mammoth to vscode package

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -4,6 +4,8 @@ import 'zx/globals'
 await fs.copy('../../node_modules/pdfjs-dist/package.json', './node_modules/pdfjs-dist/package.json', { overwrite: true })
 await $`cp -R ../../node_modules/pdfjs-dist/build ./node_modules/pdfjs-dist`
 
+await $`cp -R ../../node_modules/mammoth ./node_modules/`
+
 const pkg = await fs.readJSON('./package.json')
 pkg._enabledApiProposals = pkg.enabledApiProposals
 pkg.displayName = "GenAIScript"
