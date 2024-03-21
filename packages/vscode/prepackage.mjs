eb393bc3c290234058946aa8cab7caa4953254c6
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-21 06:47:06 +0100

    bundle mammoth in core (#281)
    * bundle mammoth
    
    * don't copy mammoth in node

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -4,9 +4,6 @@ import 'zx/globals'
 await fs.copy('../../node_modules/pdfjs-dist/package.json', './node_modules/pdfjs-dist/package.json', { overwrite: true })
 await $`cp -R ../../node_modules/pdfjs-dist/build ./node_modules/pdfjs-dist`
 
-await fs.copy('../../node_modules/mammoth/package.json', './node_modules/mammoth/package.json', { overwrite: true })
-await $`cp -R ../../node_modules/mammoth/lib ./node_modules/mammoth`
-
 const pkg = await fs.readJSON('./package.json')
 pkg._enabledApiProposals = pkg.enabledApiProposals
 pkg.displayName = "GenAIScript"
