a01ae45a13d0de193c7a3b019937d21159d19cad
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-23 12:42:55 +0000

    package treesitter

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -3,6 +3,7 @@ import 'zx/globals'
 // workaround yarn workspaces
 await fs.copy('../../node_modules/pdfjs-dist/package.json', './node_modules/pdfjs-dist/package.json', { overwrite: true })
 await $`cp -R ../../node_modules/pdfjs-dist/build ./node_modules/pdfjs-dist`
+await $`cp -R ../../node_modules/tree-sitter-wasms ./node_modules`
 
 const pkg = await fs.readJSON('./package.json')
 pkg._enabledApiProposals = pkg.enabledApiProposals
