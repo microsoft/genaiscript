bedd03df19182579a86c026285f69990691a7544
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-03-25 10:20:27 +0000

    support in runPrompt, skip in aici

--- packages/core/src/runpromptcontext.ts
+++ packages/core/src/runpromptcontext.ts
@@ -10,7 +10,7 @@ import {
 import { createDefNode } from "./filedom"
 import { MarkdownTrace } from "./trace"
 import { DEFAULT_MODEL, DEFAULT_TEMPERATURE } from "./constants"
-import { ChatCompletionMessageParam, toChatCompletionUserMessage } from "./chat"
+import { ChatCompletionAssistantMessageParam, ChatCompletionMessageParam, toChatCompletionUserMessage } from "./chat"
 import { RunTemplateOptions } from "./promptcontext"
 import { resolveLanguageModel } from "./models"
 import { renderAICI } from "./aici"
@@ -104,7 +104,7 @@ export function createRunPromptContext(
                     // todo: output processor?
                     messages.push(aici)
                 } else {
-                    const { prompt, images, errors } = await renderPromptNode(
+                    const { prompt, assistantPrompt, images, errors } = await renderPromptNode(
                         model,
                         node,
                         {
@@ -115,6 +115,8 @@ export function createRunPromptContext(
                     if (images?.length || errors?.length)
                         trace.fence({ images, errors }, "yaml")
                     messages.push(toChatCompletionUserMessage(prompt, images))
+                    if (assistantPrompt)
+                        messages.push(<ChatCompletionAssistantMessageParam>{ role: "assistant", content: assistantPrompt })
                 }
 
                 // call LLM
