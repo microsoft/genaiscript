6753adda4dc84f96bbfadd2e337a8f2123c03750
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-04-01 20:46:23 +0000

    adding treesitter querying

---
+++ packages/core/src/treesitter.ts
@@ -0,0 +1,80 @@
+import Parser from "web-tree-sitter"
+import { MarkdownTrace } from "./trace"
+import { host } from "./host"
+import { resolveFileContent } from "./file"
+
+const EXT_MAP: Record<string, string> = {
+    js: "javascript",
+    jsx: "javascript",
+    mjs: "javascript",
+    cjs: "javascript",
+    cs: "c_sharp",
+    sh: "bash",
+    py: "python",
+    rs: "rust",
+    rb: "ruby",
+    ts: "typescript",
+    tsx: "typescript",
+}
+
+async function resolveLanguage(filename: string) {
+    const ext = host.path.extname(filename).slice(1).toLowerCase()
+    const language = EXT_MAP[ext] || ext
+
+    try {
+        const url = require.resolve(
+            `tree-sitter-wasms/out/tree-sitter-${language}.wasm`
+        )
+        return url
+    } catch (e) {
+        const url = `https://unpkg.com/browse/tree-sitter-wasms@latest/out/tree-sitter-${language}.wasm`
+        return url
+    }
+}
+
+let _initPromise: Promise<void>
+async function init() {
+    if (!_initPromise) _initPromise = Parser.init()
+    await _initPromise
+}
+
+async function createParser(
+    wasmPath: string,
+    options?: { trace?: MarkdownTrace }
+) {
+    const { trace } = options || {}
+    const languageWasm = await Parser.Language.load(wasmPath)
+    const parser = new Parser()
+    parser.setLanguage(languageWasm)
+    if (trace) parser.setLogger((message) => trace?.log(message))
+    return parser
+}
+
+export async function treeSitterQuery(
+    file: LinkedFile,
+    query: string,
+    options?: { trace?: MarkdownTrace }
+) {
+    const { filename } = file
+    const { trace } = options || {}
+
+    await resolveFileContent(file)
+    if (!file.content) return undefined
+
+    try {
+        trace?.startDetails("code query")
+        trace?.fence(query, "txt")
+
+        await init()
+        const url = await resolveLanguage(filename)
+        trace?.item(`wasm: ${url}`)
+        const parser = await createParser(url, options)
+        const tree = parser.parse(file.content)
+        const lang = parser.getLanguage()
+        const q = lang.query(query)
+        const res = q.captures(tree.rootNode)
+        return res
+    } finally {
+        trace?.endDetails()
+    }
+}
