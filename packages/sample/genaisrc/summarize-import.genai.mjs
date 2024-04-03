500afa1c8c7a850cccb94476d21bb349405b9e6d
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-04-03 19:35:41 +0000

    add test import

--- packages/sample/genaisrc/summarize-import.genai.mjs
+++ packages/sample/genaisrc/summarize-import.genai.mjs
@@ -1,8 +1,12 @@
+import { parse } from "ini"
+
 script({
     title: "summarize all files using import"
 })
 
 export default function () {
+    const res = parse("x = 1\ny = 2")
+    console.log(res)
     def("FILE", env.files)
     $`Summarize each file. Be concise.`
 }
