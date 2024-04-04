b44e5389ad07c08e6531afce269bc4572921dce4
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-04-04 01:57:09 +0000

    copy the entire pdfjs-dist in vscode extension

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -1,14 +1,13 @@
-import 'zx/globals'
+import "zx/globals"
 
 // workaround yarn workspaces
-await fs.copy('../../node_modules/pdfjs-dist/package.json', './node_modules/pdfjs-dist/package.json', { overwrite: true })
-await $`cp -R ../../node_modules/pdfjs-dist/build ./node_modules/pdfjs-dist`
+await $`cp -R ../../node_modules/pdfjs-dist ./node_modules`
 await $`cp -R ../../node_modules/tree-sitter-wasms ./node_modules`
 await $`cp -R ../../node_modules/web-tree-sitter ./node_modules`
 
-const pkg = await fs.readJSON('./package.json')
+const pkg = await fs.readJSON("./package.json")
 pkg._enabledApiProposals = pkg.enabledApiProposals
 pkg.displayName = "GenAIScript"
 delete pkg.enabledApiProposals
-await fs.writeJSON('./package.json', pkg, { spaces: 4 })
+await fs.writeJSON("./package.json", pkg, { spaces: 4 })
 console.log(`cleaned package.json`)
