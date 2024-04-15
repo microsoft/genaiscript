0cb42d6ad8beb845e08ee774c705ddf851d1b624
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-04-15 15:50:12 +0000

    build script build markdown

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -45,7 +45,7 @@ export default defineConfig({
                 },
                 {
                     label: "Slides",
-                    link: "slides/default"
+                    link: "slides"
                 },
                 {
                     label: "Contributing",
---
+++ docs/src/content/docs/slides.md
@@ -0,0 +1,4 @@
+# Slides
+
+- [GenAIScript](/genaiscript/slides/default/)
+- [GenAIScript](/genaiscript/slides/vs-aua/)
