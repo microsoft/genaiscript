a0e0483b9157e922c11ebf9551daef232168b3d2
pelikhan <jhalleux@microsoft.com>
2024-04-18 15:29:37 -0700

    shrink script

--- slides/genaisrc/style-tester.genai.js
+++ slides/genaisrc/style-tester.genai.js
@@ -1,16 +1,8 @@
-// metadata and model configuration
-// https://microsoft.github.io/genaiscript/reference/scripts/metadata/
 script({
     title: "style-tester",
     system: ["system", "system.annotations", "system.functions", "system.fs_read_file"],
 })
-
-// use def to emit LLM variables
-// https://microsoft.github.io/genaiscript/reference/scripts/context/#definition-def
 const slides = def("SLIDES", env.files, { endsWith: ".md" })
-
-// use $ to output formatted text to the prompt
-// https://microsoft.github.io/genaiscript/reference/scripts/prompt/
 $`You are an expert at creating Techinal presentation using markdown/MDX
 usig the https://sli.dev/ format.
 
@@ -27,8 +19,4 @@ The slide format can reference other files as follows. Also analyze those files.
 ---
 src: path/to/slide.md
 ---
-
 `
-
-// next, "Run GenAIScript" on a file or folder
-// https://microsoft.github.io/genaiscript/getting-started/running-scripts/
