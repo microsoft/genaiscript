ab9e4269947de8dc96e921b0245984e6179e3284
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-04-22 18:43:13 +0000

    first level integration

--- packages/sample/src/run.mjs
+++ packages/sample/src/run.mjs
@@ -1,11 +1,18 @@
-import "zx/globals"
+import { execa } from "execa"
 export default async function ({ vars }) {
     const { script, spec } = vars
-    const { stdout, stderr, exitCode, failed } = await $`../cli/built/genaiscript.cjs run ${script} ${spec} --prompt`
+    const command = 'node'
+    const args = ['../cli/built/genaiscript.cjs', 'run', script, spec, '--prompt']
+    const { stdout, stderr, exitCode, failed } = await execa(command, args, {
+        cleanup: true,
+        preferLocal: true,
+        stripFinalNewline: true,
+    })
     if (failed) {
         console.error(stdout)
         console.error(stderr)
         throw new Error(`run failed ${exitCode}`)
     }
-    return stdout
+    const prompt = stdout
+    return prompt
 }
