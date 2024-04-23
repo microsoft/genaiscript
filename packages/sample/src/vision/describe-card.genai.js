50f77ccf6dcfcba2c9fba6a51230fb607a20264c
Ben Zorn <zorn@microsoft.com>
2024-04-23 00:26:02 +0000

    Sample reading contents of a business card

---
+++ packages/sample/src/vision/describe-card.genai.js
@@ -0,0 +1,16 @@
+script({
+    description: "Given an image of business card, extract the details to a csv file",
+    group: "image tools",
+    model: "gpt-4-turbo-v",
+    maxTokens: 4000,
+})
+defImages(env.files)
+
+const outputName = path.join(path.dirname(env.spec.filename), "card.csv")
+
+$`You are a helpful assistant.  You are given an image of a business 
+card.  Extract the following information:
+
+   Name, Address, Phone, Email, Company, Title, Website, Category of Business
+
+If you can't infer the category, mark it as "Unknown"`
