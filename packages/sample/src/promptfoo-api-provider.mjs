c9389fe2124420c0f67a3036660a995410b2ef87
pelikhan <jhalleux@microsoft.com>
2024-04-25 04:04:19 -0700

    try genaiscript as provider

---
+++ packages/sample/src/promptfoo-api-provider.mjs
@@ -0,0 +1,44 @@
+import { execa } from "execa"
+
+class GenAIScriptApiProvider {
+    constructor(options) {
+        this.providerId = options.id || "genaiscript"
+        this.config = options.config
+    }
+
+    id() {
+        return this.providerId
+    }
+
+    async callApi(prompt, context) {
+        const { vars } = context
+        const { script, spec } = vars
+        const command = "node"
+        const args = [
+            "../cli/built/genaiscript.cjs",
+            "run",
+            script,
+            spec,
+            "--prompt",
+        ]
+        const { stdout, stderr, exitCode, failed } = await execa(
+            command,
+            args,
+            {
+                cleanup: true,
+                preferLocal: true,
+                stripFinalNewline: true,
+            }
+        )
+        console.error(stderr)
+        const output = stdout
+        const error = failed ? `exit code ${exitCode}` : undefined
+
+        return {
+            output,
+            error
+        }
+    }
+}
+
+export default GenAIScriptApiProvider
