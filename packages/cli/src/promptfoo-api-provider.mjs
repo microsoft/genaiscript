530b9488213329c42af2d814e5725a4c40e4df12
pelikhan <jhalleux@microsoft.com>
2024-04-25 08:57:04 -0700

    added to cli

--- packages/cli/src/promptfoo-api-provider.mjs
+++ packages/cli/src/promptfoo-api-provider.mjs
@@ -3,8 +3,9 @@ import { execa } from "execa"
 // https://promptfoo.dev/docs/providers/custom-api
 class GenAIScriptApiProvider {
     constructor(options) {
-        this.providerId = options.id || "genaiscript"
         this.config = options.config
+        this.providerId =
+            options.id || `genaiscript:${this.config.model || "default"}`
     }
 
     id() {
@@ -12,24 +13,31 @@ class GenAIScriptApiProvider {
     }
 
     async callApi(prompt, context) {
-        const { model, temperature, topK, cache } = this.config
+        const { model, temperature, topK, cache, version } = this.config
         const { vars } = context
         const { files } = vars
 
-        const command = "node"
-        const args = ["../cli/built/genaiscript.cjs", "run", prompt, files]
+        const package = version ? `genaiscript@${version}` : "genaiscript"
+        const command = "npx"
+        const args = ["--yes", package, "run", prompt]
+        if (files) args.push(files)
         if (model) args.push("--model", model)
         if (temperature !== undefined) args.push("--temperature", temperature)
         if (topK !== undefined) args.push("--top_k", topK)
         if (vars.vars) args.push("--vars", vars.vars)
-        if (cache === false) args.push("--no-cache")            
+        if (cache === false) args.push("--no-cache")
+
         const { stdout, exitCode, failed } = await execa(command, args, {
             cleanup: true,
             preferLocal: true,
             stripFinalNewline: true,
         })
-        const output = stdout?.replace("Warning: TT: undefined function: 32", "")
+        const output = stdout?.replace(
+            "Warning: TT: undefined function: 32",
+            ""
+        )
         const error = failed ? `exit code ${exitCode}` : undefined
+
         return {
             output,
             error,
