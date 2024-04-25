14857c2c4c38ecb5d46a5157a0326e9c2c0c4b88
pelikhan <jhalleux@microsoft.com>
2024-04-25 04:30:00 -0700

    refactor

--- packages/sample/src/promptfoo-api-provider.mjs
+++ packages/sample/src/promptfoo-api-provider.mjs
@@ -11,32 +11,29 @@ class GenAIScriptApiProvider {
     }
 
     async callApi(prompt, context) {
+        const { model } = this.config
         const { vars } = context
         const { script, spec } = vars
-        console.log(context)
+        console.log({ prompt, context, config: this.config })
         const command = "node"
         const args = [
             "../cli/built/genaiscript.cjs",
             "run",
             script,
             spec,
+            "--model",
+            model,
         ]
-        const { stdout, stderr, exitCode, failed } = await execa(
-            command,
-            args,
-            {
-                cleanup: true,
-                preferLocal: true,
-                stripFinalNewline: true,
-            }
-        )
-        console.error(stderr)
+        const { stdout, exitCode, failed } = await execa(command, args, {
+            cleanup: true,
+            preferLocal: true,
+            stripFinalNewline: true,
+        })
         const output = stdout
         const error = failed ? `exit code ${exitCode}` : undefined
-
         return {
             output,
-            error
+            error,
         }
     }
 }
