810581af06edb7ed1781d0044010f2b301c97ed5
pelikhan <jhalleux@microsoft.com>
2024-04-30 13:41:52 -0700

    updated population strategy

--- packages/vscode/src/testcontroller.ts
+++ packages/vscode/src/testcontroller.ts
@@ -19,38 +19,40 @@ export async function activateTestController(state: ExtensionState) {
     // First, create the `resolveHandler`. This may initially be called with
     // "undefined" to ask for all tests in the workspace to be discovered, usually
     // when the user opens the Test Explorer for the first time.
-    ctrl.resolveHandler = async (test) => {
+    ctrl.resolveHandler = async (testToResolve) => {
         if (!vscode.workspace.workspaceFolders) return // handle the case of no open folders
 
         if (!state.project) await state.parseWorkspace()
-        const scripts = state.project.templates
-            .filter((t) => arrayify(t.tests)?.length)
-            .filter(
+        if (testToResolve) {
+            const script = state.project.templates.find(
                 (script) =>
-                    !test ||
                     vscode.workspace.asRelativePath(script.filename) ===
-                        vscode.workspace.asRelativePath(test.uri)
+                    vscode.workspace.asRelativePath(testToResolve.uri)
             )
-        const files: vscode.TestItem[] = []
-        for (const script of scripts) {
-            const file = getOrCreateFile(script)
-            files.push(file)
-            const tests = arrayify(script.tests).map((t, index) => {
-                const id = `${script.id}:${index}`
-                const test =
-                    ctrl.items.get(id) ??
-                    ctrl.createTestItem(
-                        `genaiscript:${script.id}:${index}`,
-                        t.description ?? `test ${index}`,
-                        vscode.Uri.file(script.filename)
-                    )
-                test.description = arrayify(t.files).join(", ")
-                testData.set(test, { script, index })
-                return test
-            })
-            file.children.replace(tests)
+            const file = await getOrCreateFile(script)
+            // TODO
+        } else {
+            const scripts = state.project.templates.filter(
+                (t) => arrayify(t.tests)?.length
+            )
+            for (const script of scripts) {
+                const file = getOrCreateFile(script)
+                const tests = arrayify(script.tests).map((t, index) => {
+                    const id = `${script.id}:${index}`
+                    const test =
+                        ctrl.items.get(id) ??
+                        ctrl.createTestItem(
+                            id,
+                            t.description ?? `test ${index}`,
+                            vscode.Uri.file(script.filename)
+                        )
+                    test.description = arrayify(t.files).join(", ")
+                    testData.set(test, { script, index })
+                    return test
+                })
+                file.children.replace(tests)
+            }
         }
-        ctrl.items.replace(files)
     }
 
     const runProfile = ctrl.createRunProfile(
@@ -71,8 +73,9 @@ export async function activateTestController(state: ExtensionState) {
             script.id,
             vscode.Uri.file(script.filename)
         )
-        file.description = script.title || script.description
+        file.description = script.title ?? script.description
         file.canResolveChildren = true
+        ctrl.items.add(file)
         return file
     }
 }
