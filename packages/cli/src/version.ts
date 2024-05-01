d19c2c722e932d682a9a698ee4475455aa1dec63
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-01 21:21:54 +0000

    fix typo

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -2,5 +2,5 @@ import packageJson from "../package.json"
 
 export const NODE_MIN_VERSION = packageJson.engines.node
 export const LLAMAINDEX_VERSION = packageJson.optionalDependencies.llamaindex
-export const PROMPTFOO_VERSION = packageJson.optionalDependencies.promptfoo
+export const PROMPTFOO_VERSION = packageJson.dependencies.promptfoo
 export const TYPESCRIPT_VERSION = packageJson.dependencies.typescript
