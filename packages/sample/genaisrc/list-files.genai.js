0ecaf6cf76530fa0047d5b89f76c103a14b54551
pelikhan <jhalleux@microsoft.com>
2024-05-01 03:56:42 -0700

    use filename

--- packages/sample/genaisrc/list-files.genai.js
+++ packages/sample/genaisrc/list-files.genai.js
@@ -1,8 +1,14 @@
-script({})
+script({
+    tests: [
+        {
+            keywords: ".genai.js"
+        }
+    ]
+})
 
 const files = await workspace.findFiles("**/*.genai.js", { readText: false })
 
 $`Select the most interresting files from the list below:
 
-${files.map((f) => f).join("\n")}
+${files.map((f) => f.filename).join("\n")}
 `
