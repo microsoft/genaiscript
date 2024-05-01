c9ad56eb14c1050c374ea0adf4d968da4030836e
pelikhan <jhalleux@microsoft.com>
2024-05-01 04:55:56 -0700

    updated list-files tests

--- packages/sample/genaisrc/list-files.genai.js
+++ packages/sample/genaisrc/list-files.genai.js
@@ -1,8 +1,13 @@
-script({})
+script({
+    model: "gpt-35-turbo",
+    tests: {
+        keywords: ".genai.js",
+    },
+})
 
 const files = await workspace.findFiles("**/*.genai.js", { readText: false })
 
-$`Select the most interresting files from the list below:
+$`Select the 3 most interresting files from the list below:
 
-${files.map((f) => f).join("\n")}
+${files.map((f) => f.filename).join("\n")}
 `
