21917c23dae2ffb89d179f02400a2f6f9ad9a354
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-02 20:43:04 +0000

    generate list of functions

--- packages/core/src/scripts.ts
+++ packages/core/src/scripts.ts
@@ -36,17 +36,33 @@ TELL THE LLM WHAT TO DO...\`
     return t
 }
 
+function scanTools(v: string) {
+    const tools: { name: string; description: string }[] = []
+    v.replace(
+        /defTool\s*\(\s*"([^"]+)"\s*,\s*"([^"]+)"/gm,
+        (m, name, description) => {
+            tools.push({ name, description })
+            return ""
+        }
+    )
+    return tools
+}
+
 export async function fixPromptDefinitions(project: Project) {
     const folders = project.folders()
     for (const folder of folders) {
         for (let [defName, defContent] of Object.entries(promptDefinitions)) {
             if (project && defName === "genaiscript.d.ts") {
                 const systems = project.templates.filter((t) => t.isSystem)
+                const tools = systems
+                    .map(({ jsSource }) => scanTools(jsSource))
+                    .flat()
 
+                // update the system prompt identifiers
                 defContent = defContent
                     .replace(
                         "type SystemPromptId = string",
-                        `type SystemPromptId = ${systems.map((s) => `"${s.id}"`).join(" | ")}`
+                        `type SystemPromptId = ${systems.map((s) => JSON.stringify(s.id)).join(" | ")}`
                     )
                     .replace(
                         "    system?: SystemPromptId[]",
@@ -55,6 +71,21 @@ export async function fixPromptDefinitions(project: Project) {
 ${systems.map((s) => `* - \`${s.id}\`: ${s.title || s.description}`).join("\n")}
 **/
     system?: SystemPromptId[]`
+                    )
+
+                // update the tool prompt identifiers
+                defContent = defContent
+                    .replace(
+                        "type SystemToolId = string",
+                        `type SystemToolId = ${tools.map((s) => JSON.stringify(s.name)).join(" | ")}`
+                    )
+                    .replace(
+                        "    tools?: SystemToolId[]",
+                        `/**
+* System tool identifiers ([reference](https://microsoft.github.io/genaiscript/reference/scripts/tools/))
+${tools.map((s) => `* - \`${s.name}\`: ${s.description}`).join("\n")}
+**/
+    tools?: SystemToolId[]`
                     )
             }
 
