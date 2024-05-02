c4c3da719d1a9bc3282e928c4096667ded771a49
pelikhan <jhalleux@microsoft.com>
2024-05-02 08:47:16 -0700

    better script

--- genaisrc/docs_sample_generator.genai.js
+++ genaisrc/docs_sample_generator.genai.js
@@ -5,15 +5,16 @@ script({
         api: {
             type: "string",
             description: "The API to search for samples",
-            default: "defData",
+            default: "defTool",
         },
     },
 })
 
-const api = env.vars.apis + ""
+const api = env.vars.api + ""
+console.log(`generating sample for ${api}`)
 
 const scripts = (
-    await workspace.findFiles("packages/sample/src/**/*genai.js")
+    await workspace.findFiles("packages/*/src/**/*genai.js")
 ).filter((f) => f.content?.includes(api))
 const { files: samples } = await retrieval.search(api, scripts)
 console.debug(samples)
@@ -21,7 +22,7 @@ console.debug(samples)
 const docs = (
     await workspace.findFiles("docs/src/content/docs/**/*.md*")
 ).filter((f) => f.content?.includes(api))
-const { files: docsSamples } = await retrieval.search(api, docs)
+const { files: docsSamples } = await retrieval.search(api, docs, { topK: 3 })
 console.debug(docsSamples)
 
 const sn = def("SAMPLES", samples, { maxTokens: 10000 })
@@ -31,10 +32,10 @@ $`
 You are an expert at writing GenAIScript programs.
 
 Write an example genaiscript program that uses \`${api}\`.
-using the information found in ${sn}.
+using the information found in ${sn} and ${dc}.
 
 - Answer with the javascript code
-- Use the information found in the ${sn} and ${dc} to generate the example
+- The generated code must use \`${api}\`
 - Keep the example simple and as short as possible
 - Generate comments for each line of code
 - Do not generate the \`script\` function call unless absolutely necessary
@@ -42,5 +43,6 @@ using the information found in ${sn}.
 - Do not use any external libraries
 - Do not use any external APIs
 - Do not use any external services
-
+- The example should have less than 5 lines of code
+- Use pseudo code if necessary
 `
