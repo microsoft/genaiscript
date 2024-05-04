5676030c847b6330a202ba08e1c6701eedda8c4c
pelikhan <jhalleux@microsoft.com>
2024-05-03 21:49:44 -0400

    udpated prompt

--- packages/sample/genaisrc/summarize-ollama-phi3.genai.js
+++ packages/sample/genaisrc/summarize-ollama-phi3.genai.js
@@ -1,7 +1,7 @@
 script({
     model: "ollama:phi3",
     title: "summarize with ollama phi3",
-    system: ["system"],
+    system: [],
     tests: {
         files: "src/rag/markdown.md",
         keywords: "markdown",
@@ -10,4 +10,4 @@ script({
 
 const file = def("FILE", env.files)
 
-$`Summarize ${file} in a single paragraph.`
+$`Summarize ${file} in a single paragraph in plaintext.`
