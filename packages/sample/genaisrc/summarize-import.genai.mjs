7ea8696a9b286f5ae41a9d4cacd04ab95b19bcef
pelikhan <jhalleux@microsoft.com>
2024-05-07 00:15:41 +0200

    updated import script

--- packages/sample/genaisrc/summarize-import.genai.mjs
+++ packages/sample/genaisrc/summarize-import.genai.mjs
@@ -1,5 +1,3 @@
-import { parse } from "ini"
-
 script({
     title: "summarize all files using import",
     tests: {
@@ -9,8 +7,6 @@ script({
 })
 
 export default async function () {
-    const res = parse("x = 1\ny = 2")
-    console.log(res)
     def("FILE", env.files)
     $`Summarize each file. Be concise.`
 }
