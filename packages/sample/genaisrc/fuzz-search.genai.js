ca85ffa3dfa377a151829987fcf29a00b835a03b
pelikhan <jhalleux@microsoft.com>
2024-05-08 01:16:47 +0200

    added test

---
+++ packages/sample/genaisrc/fuzz-search.genai.js
@@ -0,0 +1,11 @@
+script({
+    title: "fuzz search",
+    tests: {},
+})
+
+const kw = /** @type {string} */ (env.vars.keyword || "defdata")
+const allFiles = await workspace.findFiles("**/*.genai.js")
+const files = await retrieval.fuzzSearch(kw, allFiles)
+def("FILE", files, { maxTokens: 10000 })
+
+$`Use the information in FILE and generate a documentation for '${kw}' in '${kw}.md'.`
