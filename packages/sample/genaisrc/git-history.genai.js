3de733a48c890364c448e78803f25f30f34337c0
pelikhan <jhalleux@microsoft.com>
2024-05-13 10:53:47 -0700

    add shell examples

---
+++ packages/sample/genaisrc/git-history.genai.js
@@ -0,0 +1,26 @@
+// metadata and model configuration
+// https://microsoft.github.io/genaiscript/reference/scripts/metadata/
+script({ title: "git-history" })
+
+const author = env.vars.author || "pelikhan"
+const until = env.vars.until || "2023-11-15"
+defTool(
+    "git-log",
+    "Returns the list of commits for a user until a given date",
+    {
+        type: "object",
+        properties: {
+            author: { type: "string", description: "git username" },
+            until: { type: "string", description: "until date formatted as yyyy-mm-dd" },
+        },
+        required: ["author", "until"],
+    },
+    (args) => {
+        return {
+            type: "shell",
+            command: `git log --author=${args.author} --until=${args.until} --format=oneline`,
+        }
+    }
+)
+
+$`Summarize the git history of ${author} from ${until}.`
