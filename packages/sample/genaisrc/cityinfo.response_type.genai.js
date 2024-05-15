f6fca1a5048a516c33b4f3a18d6d360e3737e2ef
pelikhan <jhalleux@microsoft.com>
2024-05-15 12:47:05 -0700

    build issue

--- packages/sample/genaisrc/cityinfo.response_type.genai.js
+++ packages/sample/genaisrc/cityinfo.response_type.genai.js
@@ -1,23 +1,34 @@
 script({
     responseSchema: {
-        type: "array",
-        description:
-            "A list of cities with population and elevation information.",
-        items: {
-            type: "object",
-            description: "A city with population and elevation information.",
-            properties: {
-                name: { type: "string", description: "The name of the city." },
-                population: {
-                    type: "number",
-                    description: "The population of the city.",
-                },
-                url: {
-                    type: "string",
-                    description: "The URL of the city's Wikipedia page.",
+        type: "object",
+        required: ["cities"],
+        properties: {
+            cities: {
+                type: "array",
+                description:
+                    "A list of cities with population and elevation information.",
+                items: {
+                    type: "object",
+                    description:
+                        "A city with population and elevation information.",
+                    properties: {
+                        name: {
+                            type: "string",
+                            description: "The name of the city.",
+                        },
+                        population: {
+                            type: "number",
+                            description: "The population of the city.",
+                        },
+                        url: {
+                            type: "string",
+                            description:
+                                "The URL of the city's Wikipedia page.",
+                        },
+                    },
+                    required: ["name", "population", "url"],
                 },
             },
-            required: ["name", "population", "url"],
         },
     },
 })
