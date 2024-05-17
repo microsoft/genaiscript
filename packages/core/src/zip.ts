8ef9413e1815aa5785f567607ba007614e550b86
pelikhan <jhalleux@microsoft.com>
2024-05-17 08:29:52 -0700

    add unzip tests

--- packages/core/src/zip.ts
+++ packages/core/src/zip.ts
@@ -1,7 +1,13 @@
 import { unzipSync } from "fflate"
 import { minimatch } from "minimatch"
+import { lookupMime } from "./mime"
+import { isBinaryMimeType } from "./parser"
+import { host } from "./host"
 
-export async function unzip(data: Uint8Array, options?: ParseZipOptions) {
+export async function unzip(
+    data: Uint8Array,
+    options?: ParseZipOptions
+): Promise<WorkspaceFile[]> {
     const { glob } = options || {}
     const res = unzipSync(data, {
         filter: (file: { name: string; originalSize: number }) => {
@@ -12,5 +18,11 @@ export async function unzip(data: Uint8Array, options?: ParseZipOptions) {
             return true
         },
     })
-    return res
+    const decoder = host.createUTF8Decoder()
+    return Object.entries(res).map(([filename, data]) => {
+        const mime = lookupMime(filename)
+        if (isBinaryMimeType(mime))
+            return <WorkspaceFile>{ filename } // TODO bytes support
+        else return <WorkspaceFile>{ filename, content: decoder.decode(data) }
+    })
 }
