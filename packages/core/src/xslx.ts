e4c0a626c18c3382932928c956a241b17053dfb5
pelikhan <jhalleux@microsoft.com>
2024-05-17 04:49:20 -0700

    support range

--- packages/core/src/xslx.ts
+++ packages/core/src/xslx.ts
@@ -2,23 +2,30 @@ import { read, utils } from "xlsx"
 import { host } from "./host"
 import { logInfo } from "./util"
 
+export interface XSLXParseOptions {
+    // specific worksheet name
+    worksheet?: string
+    // Use specified range (A1-style bounded range string)
+    range?: string
+}
+
 export function XSLXParse(
     data: Uint8Array,
-    options?: { worksheet?: string }
+    options?: XSLXParseOptions
 ): object[] {
-    const { worksheet } = options || {}
+    const { worksheet, ...rest } = options || {}
     const workbook = read(data, { type: "array" })
     const sheetName = worksheet || workbook.SheetNames[0]
     const sheet = workbook.Sheets[sheetName]
     if (!sheet) throw new Error(`Sheet not found: ${sheetName}`)
 
-    const res = utils.sheet_to_json(sheet)
+    const res = utils.sheet_to_json(sheet, rest)
     return res as object[]
 }
 
 export function XSLXTryParse(
     data: Uint8Array,
-    options?: { worksheet?: string }
+    options?: XSLXParseOptions
 ): object[] {
     try {
         return XSLXParse(data, options)
