95838312b5aca8b87c4aca11f99d598823d70be7
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-20 21:25:56 +0000

    fix schema-infer test

--- packages/sample/genaisrc/schema-infer.genai.js
+++ packages/sample/genaisrc/schema-infer.genai.js
@@ -5,13 +5,13 @@ script({
         files: "src/penguins.csv",
         keywords: "species"
     }, {
-        files: "src/penguins.xslx",
+        files: "src/penguins.xlsx",
         keywords: "species"
     }]
 })
 // use def to emit LLM variables 
 // https://microsoft.github.io/genaiscript/reference/scripts/context/#definition-def
-const files = def("FILE", env.files, { endsWith: ".csv", sliceSample: 20 })
+const files = def("FILE", env.files, { sliceSample: 20 })
 
 // use $ to output formatted text to the prompt
 // https://microsoft.github.io/genaiscript/reference/scripts/prompt/
