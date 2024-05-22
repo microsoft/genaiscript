ea6603421d1694b84523096583108cf03d4f6605
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-22 19:14:31 +0000

    use tools

--- packages/sample/genaisrc/exec.genai.js
+++ packages/sample/genaisrc/exec.genai.js
@@ -1,11 +1,11 @@
 script({
     model: "gpt-3.5-turbo",
     tests: {
-        keywords: "Python"
+        keywords: ["Python", "3."]
     }
 })
-
-const version = await host.exec("python", ["--version"])
-if (version.exitCode)
-    throw new Error(`Failed to get Python version: ${version.stderr}`)
-$`Python version: ${version.stdout}`
+defTool("python_version", "determines the version of python on this system", {}, async () => {
+    const version = await host.exec("python", ["--version"])
+    return version.stdout?.replace(/^python\s/i, '') || "?"
+})
+$`What is the version of python on this machine?`
