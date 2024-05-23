da8f1df63cfb723757c5fdef0a106e555a9034ff
pelikhan <jhalleux@microsoft.com>
2024-05-23 09:43:37 -0700

    test exec of python script

--- packages/sample/genaisrc/container.genai.js
+++ packages/sample/genaisrc/container.genai.js
@@ -5,7 +5,8 @@ script({
     },
 })
 
-const container = await host.container({ image: "python:alpine" })
+const disablePurge = env.vars.purge === "no"
+const container = await host.container({ disablePurge })
 const version = await container.exec("python", ["--version"])
 if (!/^python \d/i.test(version.stdout))
     throw new Error("python --version failed")
@@ -15,7 +16,9 @@ if (version.stdout !== fversion)
     throw new Error(
         `writetext/readtext error, expected '${version.stdout}', got '${fversion}'`
     )
-
+await container.writeText("main.py", 'print("hello")')
+const hello = await container.exec("python", ["main.py"])
+if (hello.exitCode) throw new Error("python script failed")
 defTool(
     "python_version",
     "determines the version of python on this system",
