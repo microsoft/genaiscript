30a5426ce70e9675137d7fc7af3fb38ab029e61c
pelikhan <jhalleux@microsoft.com>
2024-05-24 08:55:09 -0700

    added tests

--- packages/core/src/transformers.test.ts
+++ packages/core/src/transformers.test.ts
@@ -6,6 +6,7 @@ import assert from "node:assert/strict"
 describe("sentiment function", () => {
     test("should process love", async () => {
         const result = await sentiment("i love u")
-        assert.strictEqual(result, "POSITIVE")
+        assert.strictEqual(result.label, "POSITIVE")
+        assert(result.score > 0.9)
     })
 })
