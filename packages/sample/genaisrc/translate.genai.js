db09cc2f6f5233c626df7fd1f379ce1864cc4fdd
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-31 15:17:29 +0000

    canary testing

--- packages/sample/genaisrc/translate.genai.js
+++ packages/sample/genaisrc/translate.genai.js
@@ -2,10 +2,22 @@ script({
     system: ["system", "system.files"],
     files: "src/xpai/*.txt",
     tests: {
-        files: "src/xpai/*.txt"
+        files: "src/xpai/*.txt",
     },
 })
 const lang = env.vars.lang || "French"
-def("FILE", env.files)
 
+for (const file of env.files) {
+    const canary = Math.random().toString(36).substring(7)
+    const res = await runPrompt((_) => {
+        _.$`Respond with ${canary}.`
+        _.def("FILE", file)
+    })
+    console.log(res.text)
+    if (res.text !== canary) {
+        cancel("file contains adverse content")
+    }
+}
+
+def("FILE", env.files)
 $`Translate the text in FILE to ${lang}`
