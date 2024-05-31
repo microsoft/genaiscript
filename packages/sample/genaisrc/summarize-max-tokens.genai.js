9de5a791f9af1d3c63504d3bf61418d209d7887a
pelikhan <jhalleux@microsoft.com>
2024-05-30 18:07:21 -0700

    a few more tests using gpt3.5

--- packages/sample/genaisrc/summarize-max-tokens.genai.js
+++ packages/sample/genaisrc/summarize-max-tokens.genai.js
@@ -1,5 +1,6 @@
 script({
     title: "summarize with max tokens",
+    model: "openai:gpt-3.5-turbo",
     tests: {
         files: ["src/rag/*"],
         keywords: ["markdown", "lorem", "microsoft"],
