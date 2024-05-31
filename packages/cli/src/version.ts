634c89a57435bf6a1ab16220edb16745bfc33854
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-30 17:45:45 -0700

    lazy loading of dockerode (#494)
    * lazy loading of dockerode
    
    * use gpt-3.5-turbo for slides

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -4,3 +4,4 @@ export const NODE_MIN_VERSION = packageJson.engines.node
 export const LLAMAINDEX_VERSION = packageJson.optionalDependencies.llamaindex
 export const PROMPTFOO_VERSION = packageJson.dependencies.promptfoo
 export const TYPESCRIPT_VERSION = packageJson.dependencies.typescript
+export const DOCKERODE_VERSION = packageJson.dependencies.dockerode
