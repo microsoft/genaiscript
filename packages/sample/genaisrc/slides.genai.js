290248f233890b617b56e8385db38ad1216c3563
pelikhan <jhalleux@microsoft.com>
2024-05-30 17:37:43 -0700

    use gpt-3.5-turbo for slides

--- packages/sample/genaisrc/slides.genai.js
+++ packages/sample/genaisrc/slides.genai.js
@@ -3,6 +3,7 @@ script({
     description:
         "Generate a slidedeck in markdown. Install extension 'vscode-reveal'.",
     group: "samples",
+    model: "openai:gpt-3.5-turbo",
     temperature: 0.1,
     tests: {
         files: ["src/greeter.ts"],
