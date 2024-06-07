5ed24dc301146a04d266d4e7b6695af4a38cf5f1
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-06-07 16:07:57 +0000

    success in sign with vscode

--- packages/vscode/src/azuremanager.ts
+++ packages/vscode/src/azuremanager.ts
@@ -3,20 +3,30 @@ import {
     VSCodeAzureSubscriptionProvider,
 } from "@microsoft/vscode-azext-azureauth"
 import { ExtensionState } from "./state"
+import * as vscode from "vscode"
+import { CancelError, errorMessage, isCancelError } from "genaiscript-core"
 
 export class AzureManager {
     private _vscodeAzureSubscriptionProvider:
         | VSCodeAzureSubscriptionProvider
         | undefined
 
-    constructor(readonly state: ExtensionState) {
-
-    }
+    constructor(readonly state: ExtensionState) {}
 
     async signIn(): Promise<void> {
         if (!this._vscodeAzureSubscriptionProvider)
             this._vscodeAzureSubscriptionProvider =
                 new VSCodeAzureSubscriptionProvider()
-        await signInToTenant(this._vscodeAzureSubscriptionProvider)
+
+        try {
+            const signed = await this._vscodeAzureSubscriptionProvider.signIn()
+            if (!signed) throw new CancelError("Azure sign in failed")
+        } catch (e) {
+            if (!isCancelError(e)) throw e
+
+            const msg = errorMessage(e)
+            vscode.window.showErrorMessage(msg)
+            throw new CancelError("Azure sign in failed")
+        }
     }
 }
