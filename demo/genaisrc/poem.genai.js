ecbeee4746a5c5ca247512a47310d2777ef09a59
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-06-11 21:18:08 +0000

    updated poem

--- demo/genaisrc/poem.genai.js
+++ demo/genaisrc/poem.genai.js
@@ -1,8 +1,8 @@
-script({ 
-    model: "azure:gpt2networkverification",
-    files: "web-app-basic-linux/main.bicep" })
+script({ files: "web-app-basic-linux/main.bicep" })
 def("FILE", env.files, { endsWith: ".bicep" } )
 $`
+You are an expert at Azure Bicep and an expert developer.
+
 - Write a 1 sentence short poem about FILE.
 - Use best practices to create 1 improvement in FILE. Include document URL. Use annotations format.
 
