7b8e8ac7516690068e22b2d22326f5658d3d22f4
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-06-11 04:09:08 +0000

    fixing tool resolution

--- demo/genaisrc/az-explain.genai.js
+++ demo/genaisrc/az-explain.genai.js
@@ -2,6 +2,8 @@ script({
     model: "gpt-3.5-turbo",
     temperature: 0,
     files: "web-app-basic-linux/main.bicep",
+    system: ["system", "system.explanations", "system.files", "system.annotations"],
+    tools: ["fs_find_files", "fs_read_file"]
 })
 def("BICEP", env.files)
 $`You are a Azure export using the Bicep Language.`
@@ -9,4 +11,14 @@ $`You are a Azure export using the Bicep Language.`
 $`Use best practices to reports annotations for the BICEP file.
 
 - only report errors
+- use annotations format
+- use file full names
+
+## Module resolution
+
+If you find code like
+
+    module createNic1 './nic.bicep'
+
+then the read the file content of the file nic.bicep and report any errors in that file.
 `
