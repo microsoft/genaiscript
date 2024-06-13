19cf5c0a237650ce483a629a5941779704445d5a
pelikhan <jhalleux@microsoft.com>
2024-06-13 08:32:41 -0700

    serialize as markdown

--- packages/vscode/src/notebook.ts
+++ packages/vscode/src/notebook.ts
@@ -4,15 +4,15 @@ import {
     details,
     fenceMD,
     Fragment,
-    fromHex,
     MARKDOWN_MIME_TYPE,
     stringToPos,
     TextFile,
-    toHex,
     TOOL_NAME,
-    YAML_MIME_TYPE,
     YAMLStringify,
 } from "genaiscript-core"
+
+// parser
+// https://raw.githubusercontent.com/microsoft/vscode-markdown-notebook/main/src/markdownParser.ts
 
 const NOTEBOOK_ID = "genaiscript"
 const NOTEBOOK_TYPE = "genaiscript"
@@ -44,10 +44,13 @@ function clean(o: any) {
 }
 
 export async function activateNotebook(state: ExtensionState) {
+    activateNotebookSerializer(state)
+    activateNotebookExecutor(state)
+}
+
+function activateNotebookExecutor(state: ExtensionState) {
     const { context } = state
     const { subscriptions } = context
-    const encoder = new TextEncoder()
-    const decoder = new TextDecoder()
 
     const controller = vscode.notebooks.createNotebookController(
         NOTEBOOK_ID,
@@ -166,72 +169,49 @@ export async function activateNotebook(state: ExtensionState) {
         }
     }
 
+    activateNotebookSerializer(state)
+}
+
+function activateNotebookSerializer(state: ExtensionState) {
+    const { context } = state
+    const { subscriptions } = context
+    const encoder = new TextEncoder()
+    const decoder = new TextDecoder()
+
     subscriptions.push(
         vscode.workspace.registerNotebookSerializer(
             NOTEBOOK_TYPE,
             {
-                deserializeNotebook: function (
-                    content: Uint8Array,
+                deserializeNotebook: (
+                    data: Uint8Array,
                     token: vscode.CancellationToken
-                ): vscode.NotebookData | Thenable<vscode.NotebookData> {
-                    const contents = decoder.decode(content)
+                ): vscode.NotebookData => {
+                    const content = decoder.decode(data)
 
-                    let raw: GenAINotebookCell[]
-                    try {
-                        raw = (<GenAINotebook>JSON.parse(contents)).cells
-                    } catch {
-                        raw = []
-                    }
-
-                    const cells = raw.map((item) => {
-                        const data = new vscode.NotebookCellData(
-                            item.type === "code"
-                                ? vscode.NotebookCellKind.Code
-                                : vscode.NotebookCellKind.Markup,
-                            item.source,
-                            item.type === "code" ? "javascript" : "markdown"
-                        )
-                        if (item.outputs?.length)
-                            data.outputs = item.outputs.map(
-                                (o) =>
-                                    new vscode.NotebookCellOutput(
-                                        o.items.map(
-                                            (i) =>
-                                                new vscode.NotebookCellOutputItem(
-                                                    fromHex(i.data),
-                                                    i.mime
-                                                )
-                                        )
-                                    )
-                            )
-                        return data
-                    })
+                    const cellRawData = parseMarkdown(content)
+                    const cells = cellRawData.map(
+                        (data) =>
+                            <vscode.NotebookCellData>{
+                                kind: data.kind,
+                                languageId: data.language,
+                                metadata: {
+                                    leadingWhitespace: data.leadingWhitespace,
+                                    trailingWhitespace: data.trailingWhitespace,
+                                    indentation: data.indentation,
+                                },
+                                outputs: [],
+                                value: data.content,
+                            }
+                    )
 
                     return new vscode.NotebookData(cells)
                 },
                 serializeNotebook: function (
                     data: vscode.NotebookData,
                     token: vscode.CancellationToken
-                ): Uint8Array | Thenable<Uint8Array> {
-                    const contents: GenAINotebookCell[] = []
-                    for (const cell of data.cells) {
-                        contents.push({
-                            type:
-                                cell.kind === vscode.NotebookCellKind.Code
-                                    ? "code"
-                                    : "markdown",
-                            source: cell.value,
-                            outputs: cell.outputs?.map((o) => ({
-                                items: o.items.map((i) => ({
-                                    mime: i.mime,
-                                    data: toHex(i.data),
-                                })),
-                            })),
-                        })
-                    }
-                    return encoder.encode(
-                        JSON.stringify({ cells: contents }, null, 2)
-                    )
+                ): Uint8Array {
+                    const stringOutput = writeCellsToMarkdown(data.cells)
+                    return encoder.encode(stringOutput)
                 },
             },
             { transientOutputs: false }
@@ -260,4 +240,215 @@ export async function activateNotebook(state: ExtensionState) {
             }
         )
     )
+}
+
+interface RawNotebookCell {
+    indentation?: string
+    leadingWhitespace: string
+    trailingWhitespace: string
+    language: string
+    content: string
+    kind: vscode.NotebookCellKind
+}
+
+const LANG_IDS = new Map([
+    ["bat", "batch"],
+    ["c++", "cpp"],
+    ["js", "javascript"],
+    ["ts", "typescript"],
+    ["cs", "csharp"],
+    ["py", "python"],
+    ["py2", "python"],
+    ["py3", "python"],
+])
+const LANG_ABBREVS = new Map(
+    Array.from(LANG_IDS.keys()).map((k) => [LANG_IDS.get(k), k])
+)
+
+interface ICodeBlockStart {
+    langId: string
+    indentation: string
+}
+
+/**
+ * Note - the indented code block parsing is basic. It should only be applied inside lists, indentation should be consistent across lines and
+ * between the start and end blocks, etc. This is good enough for typical use cases.
+ */
+function parseCodeBlockStart(line: string): ICodeBlockStart | null {
+    const match = line.match(/(    |\t)?```(\S*)/)
+    return (
+        match && {
+            indentation: match[1],
+            langId: match[2],
+        }
+    )
+}
+
+function isCodeBlockStart(line: string): boolean {
+    return !!parseCodeBlockStart(line)
+}
+
+function isCodeBlockEndLine(line: string): boolean {
+    return !!line.match(/^\s*```/)
+}
+
+function parseMarkdown(content: string): RawNotebookCell[] {
+    const lines = content.split(/\r?\n/g)
+    let cells: RawNotebookCell[] = []
+    let i = 0
+
+    // Each parse function starts with line i, leaves i on the line after the last line parsed
+    for (; i < lines.length; ) {
+        const leadingWhitespace = i === 0 ? parseWhitespaceLines(true) : ""
+        if (i >= lines.length) {
+            break
+        }
+        const codeBlockMatch = parseCodeBlockStart(lines[i])
+        if (codeBlockMatch) {
+            parseCodeBlock(leadingWhitespace, codeBlockMatch)
+        } else {
+            parseMarkdownParagraph(leadingWhitespace)
+        }
+    }
+
+    function parseWhitespaceLines(isFirst: boolean): string {
+        let start = i
+        const nextNonWhitespaceLineOffset = lines
+            .slice(start)
+            .findIndex((l) => l !== "")
+        let end: number // will be next line or overflow
+        let isLast = false
+        if (nextNonWhitespaceLineOffset < 0) {
+            end = lines.length
+            isLast = true
+        } else {
+            end = start + nextNonWhitespaceLineOffset
+        }
+
+        i = end
+        const numWhitespaceLines = end - start + (isFirst || isLast ? 0 : 1)
+        return "\n".repeat(numWhitespaceLines)
+    }
+
+    function parseCodeBlock(
+        leadingWhitespace: string,
+        codeBlockStart: ICodeBlockStart
+    ): void {
+        const language =
+            LANG_IDS.get(codeBlockStart.langId) || codeBlockStart.langId
+        const startSourceIdx = ++i
+        while (true) {
+            const currLine = lines[i]
+            if (i >= lines.length) {
+                break
+            } else if (isCodeBlockEndLine(currLine)) {
+                i++ // consume block end marker
+                break
+            }
+
+            i++
+        }
+
+        const content = lines
+            .slice(startSourceIdx, i - 1)
+            .map((line) =>
+                line.replace(new RegExp("^" + codeBlockStart.indentation), "")
+            )
+            .join("\n")
+        const trailingWhitespace = parseWhitespaceLines(false)
+        cells.push({
+            language,
+            content,
+            kind: vscode.NotebookCellKind.Code,
+            leadingWhitespace: leadingWhitespace,
+            trailingWhitespace: trailingWhitespace,
+            indentation: codeBlockStart.indentation,
+        })
+    }
+
+    function parseMarkdownParagraph(leadingWhitespace: string): void {
+        const startSourceIdx = i
+        while (true) {
+            if (i >= lines.length) {
+                break
+            }
+
+            const currLine = lines[i]
+            if (currLine === "" || isCodeBlockStart(currLine)) {
+                break
+            }
+
+            i++
+        }
+
+        const content = lines.slice(startSourceIdx, i).join("\n")
+        const trailingWhitespace = parseWhitespaceLines(false)
+        cells.push({
+            language: "markdown",
+            content,
+            kind: vscode.NotebookCellKind.Markup,
+            leadingWhitespace: leadingWhitespace,
+            trailingWhitespace: trailingWhitespace,
+        })
+    }
+
+    return cells
+}
+
+function writeCellsToMarkdown(
+    cells: ReadonlyArray<vscode.NotebookCellData>
+): string {
+    let result = ""
+    for (let i = 0; i < cells.length; i++) {
+        const cell = cells[i]
+        if (i === 0) {
+            result += cell.metadata?.leadingWhitespace ?? ""
+        }
+
+        if (cell.kind === vscode.NotebookCellKind.Code) {
+            const indentation = cell.metadata?.indentation || ""
+            const languageAbbrev =
+                LANG_ABBREVS.get(cell.languageId) ?? cell.languageId
+            const codePrefix = indentation + "```" + languageAbbrev + "\n"
+            const contents = cell.value
+                .split(/\r?\n/g)
+                .map((line) => indentation + line)
+                .join("\n")
+            const codeSuffix = "\n" + indentation + "```"
+
+            result += codePrefix + contents + codeSuffix
+        } else {
+            result += cell.value
+        }
+
+        result += getBetweenCellsWhitespace(cells, i)
+    }
+    return result
+}
+
+function getBetweenCellsWhitespace(
+    cells: ReadonlyArray<vscode.NotebookCellData>,
+    idx: number
+): string {
+    const thisCell = cells[idx]
+    const nextCell = cells[idx + 1]
+
+    if (!nextCell) {
+        return thisCell.metadata?.trailingWhitespace ?? "\n"
+    }
+
+    const trailing = thisCell.metadata?.trailingWhitespace
+    const leading = nextCell.metadata?.leadingWhitespace
+
+    if (typeof trailing === "string" && typeof leading === "string") {
+        return trailing + leading
+    }
+
+    // One of the cells is new
+    const combined = (trailing ?? "") + (leading ?? "")
+    if (!combined || combined === "\n") {
+        return "\n\n"
+    }
+
+    return combined
 }
