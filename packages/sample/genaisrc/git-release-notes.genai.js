ef804c2a0851e27d234cdaa4d352c42ea9b3bee3
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-06-21 03:59:51 +0000

    enable caching on release generator

--- packages/sample/genaisrc/git-release-notes.genai.js
+++ packages/sample/genaisrc/git-release-notes.genai.js
@@ -1,5 +1,6 @@
 script({ system: ["system"], temperature: 1, model: "openai:gpt-4-32k" })
 // find previous tag
+const pkg = JSON.parse((await workspace.readText("package.json")).content)
 const { stdout: tag } = await host.exec("git", [
     "describe",
     "--tags",
@@ -28,12 +29,14 @@ You are an expert software developer and release manager.
 ## Task
 
 Generate a clear, exciting, relevant, useful release notes
-for the upcoming release of your software. The commits in the release are in COMMITS.
+for the upcoming release ${pkg.version} of your software. The commits in the release are in COMMITS.
 The diff of the changes are in DIFF.
 
 ## Guidelines
 
+- tell a story about the changes
 - use emojis
+- do NOT give a commit overview
 - add deep links to commits if possible. The repository is hosted at https://github.com/microsoft/genaiscript
 
 `
