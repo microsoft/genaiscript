ad15ccdc2ef533f1c71616244132f5e76b76ee83
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-06-21 13:20:04 +0000

    adding tests and docs for csvs

---
+++ packages/sample/genaisrc/csv.genai.js
@@ -0,0 +1,31 @@
+script({
+    files: "src/penguins.csv",
+    tests: {},
+    model: "openai:gpt-3.5-turbo",
+})
+
+def("DATA", env.files, { sliceSample: 3 })
+def("DATA", env.files, { sliceHead: 3 })
+def("DATA", env.files, { sliceTail: 3 })
+
+const csv = env.files[0].content
+const rows = CSV.parse(csv)
+const prows = parsers.CSV(csv)
+if (JSON.stringify(rows) !== JSON.stringify(prows)) throw new Error("csv error")
+
+const srows = CSV.parse(
+    `A|1
+    B|2
+    C|3`,
+    { delimiter: ",", headers: ["name", "value"] }
+)
+if (
+    JSON.stringify(srows) ===
+    JSON.stringify([
+        { name: "A", value: 1 },
+        { name: "B", value: 2 },
+    ])
+)
+    throw new Error("csv error")
+
+$`Describe the data in DATA.`
