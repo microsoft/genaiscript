9df798d94e0ad3f9ac07209225d5448fe2daa621
pelikhan <jhalleux@microsoft.com>
2024-06-26 12:12:00 -0700

    default files on phi3

--- packages/sample/genaisrc/summarize-ollama-phi3.genai.js
+++ packages/sample/genaisrc/summarize-ollama-phi3.genai.js
@@ -2,6 +2,7 @@ script({
     model: "ollama:phi3",
     title: "summarize with ollama phi3",
     system: [],
+    files: "src/rag/markdown.md",
     tests: {
         files: "src/rag/markdown.md",
         keywords: "markdown",
