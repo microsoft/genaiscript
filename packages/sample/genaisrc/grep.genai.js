688fc0115e029187895248756bf2ed73824e92b5
pelikhan <jhalleux@microsoft.com>
2024-07-08 21:21:18 +0200

    grep

--- packages/sample/genaisrc/grep.genai.js
+++ packages/sample/genaisrc/grep.genai.js
@@ -1,12 +1,11 @@
 script({
     title: "grep search",
     model: "gpt-3.5-turbo",
-    tests: {},
+    tests: {},    
 })
 
 const kw = env.vars.keyword || "defdata"
 const files = await workspace.grep(kw, "**/*.genai.{js,mjs}")
 def("FILE", files, { maxTokens: 1000 })
-
-$`Use the information in FILE and generate a documentation 
-for '${kw}' in '${kw}.md'.`
+$`Use the information in FILE and generate a documentation page
+for '${kw}' in './${kw}.md'.`
