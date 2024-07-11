3d5a08ee6bd7cc0a2f04cf03f01fa7211b9e654a
pelikhan <jhalleux@microsoft.com>
2024-07-11 05:09:44 +0200

    use defFileOutput in img-alt-text

--- docs/genaisrc/image-alt-text.genai.js
+++ docs/genaisrc/image-alt-text.genai.js
@@ -1,24 +1,19 @@
 script({
     title: "Image Alt Text generator",
     description: "Generate alt text for images",
-    model: "gpt-4-turbo-v",
+    model: "openai:gpt-4-turbo-v",
     group: "docs",
     maxTokens: 4000,
-    temperature: 0
+    temperature: 0,
 })
 
+// input
 const file = env.files[0]
-
-// skip if alt-text file already exists
-const txt = await workspace.readText(file.filename + ".txt")
-if (txt.content)
-    cancel("alt text file already exists")
-
 // context
 defImages(file)
 // task
 $`You are an expert in assistive technology. You will analyze each image 
 and generate a description alt text for the image.
-
-Save the alt text in a file called "${file.filename + ".txt"}".
-`
+- Do not include Alt text in the description.`
+// output
+defFileOutput(file.filename + ".txt", `Alt text for image ${file.filename}`)
