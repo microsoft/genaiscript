d4e7011bc8f41bf3d48734f0a1e3426711ada862
pelikhan <jhalleux@microsoft.com>
2024-08-09 09:50:40 -0700

    transfer of notebook from docs

--- packages/vscode/prepackage.mjs
+++ packages/vscode/prepackage.mjs
@@ -7,4 +7,5 @@ delete pkg.enabledApiProposals
 await fs.writeJSON("./package.json", pkg, { spaces: 4 })
 await fs.rm("genaiscript.vsix", { force: true })
 await fs.rm("genaiscript.manifest", { force: true })
+await fs.cpSync("../../docs/src/content/docs/getting-started/tutorial.md", "./tutorial.md")
 console.log(`cleaned package.json`)
