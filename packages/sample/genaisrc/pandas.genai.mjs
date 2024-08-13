e67ef5ce7cd39660664195bb43fdce8fd6a8bae3
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-13 16:36:37 +0000

    pand test

--- packages/sample/genaisrc/pandas.genai.mjs
+++ packages/sample/genaisrc/pandas.genai.mjs
@@ -1,7 +1,19 @@
-const container = await host.container({ image: "python:3", networkEnabled: true })
-await container.exec("pip", ["install", "--root-user-action", "ignore", "pandas"])
-await container.writeText("main.py", `import pandas as pd
+script({ tests: {} })
+const container = await host.container({
+    image: "python:3.12",
+    networkEnabled: true,
+})
+await container.exec("pip", [
+    "install",
+    "--root-user-action",
+    "ignore",
+    "pandas",
+])
+await container.writeText(
+    "main.py",
+    `import pandas as pd
 print(pd)
-`)
-const res = await container.exec('python', ["main.py"])
-console.log(YAML.stringify(res))
+`
+)
+const res = await container.exec("python", ["main.py"])
+if (res.failed) throw new Error("import failed")
