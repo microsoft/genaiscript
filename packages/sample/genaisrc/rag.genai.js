d6a5d6f0726cea9eba072797cbe692f778d5e48a
pelikhan <jhalleux@microsoft.com>
2024-08-16 12:15:51 -0700

    configurable vector search

--- packages/sample/genaisrc/rag.genai.js
+++ packages/sample/genaisrc/rag.genai.js
@@ -16,21 +16,29 @@ def(
     "MARKDOWN",
     await retrieval.vectorSearch(
         "markdown",
-        env.files.filter((f) => f.filename.endsWith(".md")), { embeddingsModel }
+        env.files.filter((f) => f.filename.endsWith(".md")),
+        { embeddingsModel }
     )
 )
 def(
     "PDF",
     await retrieval.vectorSearch(
         "lorem",
-        env.files.filter((f) => f.filename.endsWith(".pdf")), { embeddingsModel }
+        env.files.filter((f) => f.filename.endsWith(".pdf")),
+        { embeddingsModel }
     )
 )
 def(
     "WORD",
     await retrieval.vectorSearch(
         "word",
-        env.files.filter((f) => f.filename.endsWith(".docx")), { embeddingsModel }
+        env.files.filter((f) => f.filename.endsWith(".docx")),
+        { embeddingsModel }
     )
 )
-def("ALL", await retrieval.vectorSearch("lorem", env.files), { embeddingsModel })
+def(
+    "ALL",
+    await retrieval.vectorSearch("lorem", env.files, {
+        embeddingsModel,
+    })
+)
