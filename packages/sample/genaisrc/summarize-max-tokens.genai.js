4d2a5c9c952b3bd940ce26ab3cdd2d9db0a954bd
pelikhan <jhalleux@microsoft.com>
2024-08-19 14:20:15 -0700

    fix parmeters test case

--- packages/sample/genaisrc/summarize-max-tokens.genai.js
+++ packages/sample/genaisrc/summarize-max-tokens.genai.js
@@ -1,12 +1,13 @@
 script({
     title: "summarize with max tokens",
     model: "openai:gpt-3.5-turbo",
+    files: ["src/rag/*"],
     tests: {
         files: ["src/rag/*"],
         keywords: ["markdown", "lorem", "microsoft"],
     },
 })
 
-def("FILE", env.files, { maxTokens: 20 })
+def("FILE", env.files, { maxTokens: 40 })
 
-$`Summarize each file. Be concise.`
+$`Extract keywords for the contents of FILE.`
--- packages/sample/genaisrc/summary-of-summary-phi3.genai.js
+++ packages/sample/genaisrc/summary-of-summary-phi3.genai.js
@@ -1,7 +1,7 @@
 script({
     model: "ollama:phi3",
     title: "summary of summary - phi3",
-    files: ["src/rag/*.md"], 
+    files: ["src/rag/*.md"],
     tests: {
         files: ["src/rag/*.md"],
         keywords: ["markdown", "lorem", "microsoft"],
@@ -13,11 +13,11 @@ for (const file of env.files) {
     const { text } = await runPrompt(
         (_) => {
             _.def("FILE", file)
-            _.$`Summarize the FILE and respond in plain text with one paragraph. Be consice. Ensure that summary is consistent with the content of FILE.`
+            _.$`Extract keywords for the contents of FILE.`
         },
         { model: "ollama:phi3", cacheName: "summary_phi3" }
     )
     def("FILE", { ...file, content: text })
 }
 // use summary
-$`Summarize FILE with short sentence.`
+$`Extract keywords for the contents of FILE.`
