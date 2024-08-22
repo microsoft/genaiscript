ae37058b9520d9407991b996ef04c049a100bd5c
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-22 15:02:26 -0700

    Blog ideation (#643)
    * refining blog ideation
    
    * refine more blog entry
    
    * compile optional
    
    * removing extra files
    
    * don't generate snippets in docs folder

--- genaisrc/blog-generator.genai.mjs
+++ genaisrc/blog-generator.genai.mjs
@@ -1,6 +1,7 @@
 script({
     description: "Generate a blog post for Dev.to from the documentation",
     model: "openai:gpt-4o",
+    system: [],
     tools: ["fs"],
     parameters: {
         theme: {
@@ -38,7 +39,7 @@ Use these files to help you generate a topic for the blog post.
 `
         },
         {
-            model: "openai:gpt-4o",
+            label: "generate theme",
             temperature: 1,
             system: [
                 "system.tools",
@@ -51,6 +52,73 @@ Use these files to help you generate a topic for the blog post.
     topic = res.text
 }
 
+// generate a working code snippet from topic
+let snippet
+{
+    const { text, fences, error } = await runPrompt(
+        (_) => {
+            _.defTool(
+                "genaiscript_typecheck",
+                "Validates the syntax and type checks a GenAIScript source code",
+                {
+                    source: {
+                        type: "string",
+                        description: "The GenAIScript javascript source code",
+                    },
+                },
+                async ({ source }) => {
+                    // write file to a scratch folder
+                    console.log(`checking source code`)
+                    await workspace.writeText(
+                        "temp/blog-post/script.genai.mjs",
+                        source
+                    )
+                    return await host.exec("node", [
+                        "packages/cli/built/genaiscript.cjs",
+                        "scripts",
+                        "compile",
+                        "temp/blog-post",
+                    ])
+                }
+            )
+
+            _.def("TOPIC", topic)
+            _.$`You are a JavaScript developer expert on GenAIScript (https://microsoft.github.io/genaiscript).
+
+        # Task
+        
+        - Generate a GenAISCript source code that implements the idea described in TOPIC.
+        - Validate syntax and checking with genaiscript_typecheck.
+        - Respond ONLY with the JavaScript source code. Do NOT fence code in markdown. Do not add text around code.
+        
+        # Information
+        
+        Use these files to help you generate a topic for the blog post.
+ 
+        - the code will be executed in node.js by the GenAIScript CLI
+        - the genaiscript type definition: genaiscript/genaiscript.d.ts. Assume that all globals are ambient. Do not import or require genaiscript module.
+        - the genaiscript samples: packages/sample/src/*.genai.*
+        - the documentation: docs/src/content/docs/**/*.md*
+        - the online documentation: https://microsoft.github.io/genaiscript/
+        `
+        },
+        {
+            label: "generate script",
+            system: [
+                "system.tools",
+                "system.fs_find_files",
+                "system.fs_read_file",
+            ],
+            temperature: 0.5,
+        }
+    )
+    if (error) throw error
+    snippet =
+        fences.find(
+            ({ language }) => language === "js" || language === "javascript"
+        )?.content ?? text
+}
+
 // generate a blog post
 const today = new Date()
 const yyyy = today.getFullYear()
@@ -58,25 +126,25 @@ const mm = String(today.getMonth() + 1).padStart(2, "0")
 const dd = String(today.getDate()).padStart(2, "0")
 const formattedDate = `${yyyy}-${mm}-${dd}`
 def("TOPIC", topic)
+def("SNIPPET", snippet)
 
 $`
 # Task
 
-Generate a blog post for the web site https://dev.to on the topic of using GenAIScript script 
-to solve the task described in TOPIC.
+Generate a blog post that explains how to write the code in SNIPPET.
 
 The main purpose is to create a genaiscript prompt generation script.
+Respond with the markdown content of the blog post.
 
 # Writing style and instructions
 
 - generate the blog post content, nothing else
-- save the generated markdown to a new file under the docs/src/content/docs/blog folder. THIS IS IMPORTANT
 - use a clear and engaging tone
-- illustrate with code examples
+- explain each line of code separately, link to the documentation if possible
 - title should be click-bait, use quotes (") around title
 - do NOT generate cover_image
 - the script will be execute by GenAIScript CLI
-- the TypeScript API is defined in the file genaisrc/genaiscript.d.ts
+- the TypeScript API is defined in the file genaisrc/genaiscript.d.ts. Assume that all globals are ambient. Do not import or require genaiscript module.
 - generate a single file for the blog post, do NOT generate other files
 - examples of GenAIScript code are in folder packages/sample/src/**/*.genai.*js
 - do NOT explain how to install GenAIScript or the GenAIScript CLI
@@ -97,17 +165,20 @@ You can extract information from the following files:
 - the online documentation: https://microsoft.github.io/genaiscript/
 
 `
-
-defFileOutput("docs/src/content/docs/blog/*.md", "The generated blog post")
 defOutputProcessor((output) => {
-    if (!Object.keys(output.fileEdits || {}).length) {
-        const fence = output.fences.find((f) => f.language === "markdown")
-        if (fence) {
-            const files = {
-                [`docs/src/content/docs/blog/unnamed-${formattedDate}.md`]:
-                    fence.content,
-            }
-            return { files }
-        }
+    let md = output.text
+    if (/\`\`\`markdown\n/.test(md)) {
+        md = md.replace(/\`\`\`markdown\n/g, "").replace(/\`\`\`\n?$/g, "")
+    }
+    const fm = parsers.frontmatter(md)
+    const fn =
+        `docs/src/content/docs/blog/${fm.title.replace(/[^a-z0-9]+/gi, "-")}.md`.toLocaleLowerCase()
+    const sn =
+        `packages/sample/genaisrc/blog/${fm.title.replace(/[^a-z0-9]+/gi, "-")}.genai.mjs`.toLocaleLowerCase()
+    return {
+        files: {
+            [fn]: md,
+            [sn]: snippet,
+        },
     }
 })
