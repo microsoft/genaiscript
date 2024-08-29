c58210ff5a8e46ac4a9a00c4f8097db9cdcd8588
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-29 18:26:47 +0000

    Add keys method to WorkspaceFileCache and update documentation to reflect changes

--- packages/sample/src/vision/genaiscript.d.ts
+++ packages/sample/src/vision/genaiscript.d.ts
@@ -529,6 +529,11 @@ interface WorkspaceFileCache<K, V> {
     set(key: K, value: V): Promise<void>
 
     /**
+     * List of keys
+     */
+    keys(): Promise<K[]>
+
+    /**
      * List the values in the cache.
      */
     values(): Promise<V[]>
