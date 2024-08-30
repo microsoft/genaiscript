4190875324c851d8af81f90a7569ce432976095c
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-30 14:34:46 +0000

    Refactor codeQuery to support multiple files and serialize results in YAML format

--- packages/cli/src/codequery.ts
+++ packages/cli/src/codequery.ts
@@ -1,13 +1,26 @@
 import { resolveFileContent } from "../../core/src/file"
-import { treeSitterQuery } from "../../core/src/treesitter"
+import {
+    serializeQueryCapture,
+    treeSitterQuery,
+} from "../../core/src/treesitter"
+import { YAMLStringify } from "../../core/src/yaml"
+import { host } from "../../core/src/host"
+import { logVerbose } from "../../core/src/util"
 
-export async function codeQuery(file: string, query: string) {
-    const f: WorkspaceFile = { filename: file, content: undefined }
-    await resolveFileContent(f)
-    const res = await treeSitterQuery(f, query)
-    const captures = res
-        .map(({ name, node }) => `;;; ${name}\n${node.toString()}`)
-        .join("\n")
+export async function codeQuery(files: string, query: string) {
+    const ffs = await host.findFiles(files, {
+        applyGitIgnore: true,
+    })
+    const captures: any[] = []
 
-    console.log(captures)
+    for (const filename of ffs) {
+        logVerbose(`scanning ${filename}`)
+        const f: WorkspaceFile = { filename, content: undefined }
+        await resolveFileContent(f)
+        if (!f.content) continue
+        const res = await treeSitterQuery(f, query)
+        captures.push(...res.map((r) => serializeQueryCapture(f.filename, r)))
+    }
+
+    console.log(YAMLStringify(captures))
 }
