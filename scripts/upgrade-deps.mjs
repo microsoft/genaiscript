587c0720f56a8cdafc804b091587f6859bba8da2
pelikhan <jhalleux@microsoft.com>
2024-09-05 05:27:10 -0700

    Exit script early if no dependencies were updated in upgrade-deps script

--- scripts/upgrade-deps.mjs
+++ scripts/upgrade-deps.mjs
@@ -16,6 +16,13 @@ const branch = `deps/${new Date().toISOString().replace(/[^0-9]/g, "-")}`
 
 await $`npx --yes ncu -u --deep ${autos.join(" ")}`
 await $`yarn install:force`
+
+const status = await $`git status --porcelain`
+if (!status.stdout.trim()) {
+    console.log("No changes, exiting")
+    process.exit(0)
+}
+
 await $`yarn typecheck`
 await $`yarn compile`
 await $`git checkout -b ${branch}`
