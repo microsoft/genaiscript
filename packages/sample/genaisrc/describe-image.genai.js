03321a0fb38aa37440a651386dac7f288e021124
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-09-12 10:19:34 +0000

    Update describe-image.genai.js to support multiple images and enhance package.json to include vision group in tests

--- packages/sample/genaisrc/describe-image.genai.js
+++ packages/sample/genaisrc/describe-image.genai.js
@@ -4,11 +4,20 @@ script({
     group: "vision",
     maxTokens: 4000,
     system: [],
-    files: "src/robots.jpg",
-    tests: {
-        files: "src/robots.jpg",
-        facts: "there are 3 robots on the picture",
-    },
+    files: [
+        "src/robots.jpg",
+        "https://github.com/microsoft/genaiscript/blob/main/docs/public/images/logo.png?raw=true",
+    ],
+    tests: [
+        {
+            files: "src/robots.jpg",
+            keywords: "robot",
+        },
+        {
+            files: "https://github.com/microsoft/genaiscript/blob/main/docs/public/images/logo.png?raw=true",
+            keywords: "yellow",
+        },
+    ],
 })
 
 $`Return the list of objects in the images.`
