7453886eab53877ce6bac10f83a3025aaa56125a
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-09-24 22:47:26 +0000

    Add new dependencies to settings and define VSCODE_CLI_VERSION in version.ts

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -4,6 +4,7 @@ import packageJson from "../package.json"
  * The current version of the core package.
  */
 export const CORE_VERSION = packageJson.version
+export const VSCODE_CLI_VERSION = "1.55.5"
 
 /**
  * GitHub repository URL.
