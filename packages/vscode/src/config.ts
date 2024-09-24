ef3c94430badd488a858d652959622a642275ec3
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-09-24 22:48:02 +0000

    Add fallback to VSCODE_CLI_VERSION when resolving CLI version in config.ts

--- packages/vscode/src/config.ts
+++ packages/vscode/src/config.ts
@@ -4,14 +4,16 @@ import {
     VSCODE_CONFIG_CLI_PATH,
     VSCODE_CONFIG_CLI_VERSION,
 } from "../../core/src/constants"
-import { CORE_VERSION } from "../../core/src/version"
+import { CORE_VERSION, VSCODE_CLI_VERSION } from "../../core/src/version"
 import { semverParse, semverSatisfies } from "../../core/src/semver"
 
 export async function resolveCli() {
     const config = vscode.workspace.getConfiguration(TOOL_ID)
     const cliPath = config.get(VSCODE_CONFIG_CLI_PATH) as string
     const cliVersion =
-        (config.get(VSCODE_CONFIG_CLI_VERSION) as string) || CORE_VERSION
+        (config.get(VSCODE_CONFIG_CLI_VERSION) as string) ||
+        VSCODE_CLI_VERSION ||
+        CORE_VERSION
     const gv = semverParse(CORE_VERSION)
     if (!semverSatisfies(cliVersion, ">=" + gv.major + "." + gv.minor))
         vscode.window.showWarningMessage(
