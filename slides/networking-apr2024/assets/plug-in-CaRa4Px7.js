85bd27c52f7d516dc0abc1549c9fca69bfd425c4
pelikhan <pelikhan@users.noreply.github.com>
2024-10-21 16:50:40 +0000

    Deploying to gh-pages from @ microsoft/genaiscript@25e80a88c8bfbfd1bbf2c9405266adb468df865b 🚀

---
+++ slides/networking-apr2024/assets/plug-in-CaRa4Px7.js
@@ -0,0 +1,1 @@
+const i="/genaiscript/slides/networking-apr2024/plug-in.png";export{i as _};
---
+++ slides/pnw-plse-may2024/assets/slidev/default-bGaesGT-.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-CbVteFyA.js";import{_ as r}from"../index-BIC92ghI.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
