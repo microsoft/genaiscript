baf9a7a8cad921e4f127eafee4a8099bbe9f7010
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-11-08 19:58:45 +0000

    added guide

--- packages/sample/genaisrc/guard.genai.mjs
+++ packages/sample/genaisrc/guard.genai.mjs
@@ -1,5 +1,4 @@
 // iterate over files and check if they are safe using llama-guard3:8b (https://ollama.com/library/llama-guard3)
-const unsafes = []
 for (const file of env.files) {
     const { text } = await prompt`${file}`.options({
         model: "ollama:llama-guard3:8b",
@@ -8,10 +7,6 @@ for (const file of env.files) {
         system: [],
     })
     const safe = /safe/.test(text) && !/unsafe/.test(text)
-    if (!safe) {
+    if (!safe)
         console.error(text)
-        unsafes.push(file.filename)
-    }
 }
-
-console.error(`unsafe:\n${unsafes.join("\n")}`)
