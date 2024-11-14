f9ab3bc8cfb1bf37fbc75c9ec514aeb672c516bb
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-11-14 21:15:53 +0000

    added data analyst examples

--- packages/sample/genaisrc/copilot/dataanalyst.genai.mjs
+++ packages/sample/genaisrc/copilot/dataanalyst.genai.mjs
@@ -1,3 +1,12 @@
-def("DATA", env.files)
+script({
+    tools: [
+        "fs_read_file",
+        "python_code_interpreter_copy_files_to_container",
+        "python_code_interpreter_read_file",
+        "python_code_interpreter_run",
+    ],
+})
+def("DATA", env.files.map(({ filename }) => filename).join("\n"))
 def("QUESTION", env.vars.question)
-$``
+
+$`Run python code to answer the data analyst question in QUESTION using the data in DATA.`
