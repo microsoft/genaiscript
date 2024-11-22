a0cf652227c26b3fa75f7b4dc74f053e725634f0
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-11-22 13:59:03 +0000

    feat: 🌟 add poem generation in TypeScript using context

--- packages/sample/genaisrc/transformers.genai.mjs
+++ packages/sample/genaisrc/transformers.genai.mjs
@@ -10,4 +10,13 @@ const res = await runPrompt(
         model: "transformers:onnx-community/Qwen2.5-Coder-0.5B-Instruct:q4",
     }
 )
+const res2 = await runPrompt(
+    (ctx) => {
+        ctx.$`Write a poem in typescript.`
+    },
+    {
+        cache: "trfs",
+        model: "transformers:onnx-community/Qwen2.5-Coder-0.5B-Instruct:q4",
+    }
+)
 $`Write a poem with 2 paragraphs.`
