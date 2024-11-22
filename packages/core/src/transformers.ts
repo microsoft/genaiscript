26482c49afe453cd56d45883f892eb0b256fa98c
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-11-22 13:55:43 +0000

    feat: add caching and improve generator handling 🚀

--- packages/core/src/transformers.ts
+++ packages/core/src/transformers.ts
@@ -5,12 +5,19 @@ import type {
     Chat,
     Message,
     TextGenerationOutput,
+    TextGenerationPipeline,
 } from "@huggingface/transformers"
 import { NotSupportedError } from "./error"
 import { ChatCompletionMessageParam, ChatCompletionResponse } from "./chattypes"
 import { deleteUndefinedValues, dotGenaiscriptPath, logVerbose } from "./util"
 import { parseModelIdentifier } from "./models"
 import prettyBytes from "pretty-bytes"
+import { hash } from "./crypto"
+import {
+    ChatCompletionRequestCacheKey,
+    getChatCompletionCache,
+} from "./chatcache"
+import { PLimitPromiseQueue } from "./concurrency"
 
 type GeneratorProgress =
     | {
@@ -24,18 +31,44 @@ type GeneratorProgress =
           loaded: number
           total: number
       }
+    | { status: "ready"; task: string; model: string }
 
 function progressBar() {
     const progress: Record<string, number> = {}
     return (cb: GeneratorProgress) => {
-        if (cb.status !== "progress") return
-        const p = progress[cb.file] || 0
-        const cp = Math.floor(cb.progress)
-        if (cp > p) {
-            progress[cb.file] = cp
-            logVerbose(`${cb.file}: ${cp}% (${prettyBytes(cb.loaded)})`)
+        switch (cb.status) {
+            case "progress":
+                const p = progress[cb.file] || 0
+                const cp = Math.floor(cb.progress)
+                if (cp > p + 5) {
+                    progress[cb.file] = cp
+                    logVerbose(`${cb.file}: ${cp}% (${prettyBytes(cb.loaded)})`)
+                }
+                break
+            case "ready": {
+                logVerbose(`model ${cb.model} ready`)
+                logVerbose(``)
+                break
+            }
         }
     }
+}
+
+const generators: Record<string, Promise<TextGenerationPipeline>> = {}
+const generationQueue = new PLimitPromiseQueue(1)
+
+async function loadGenerator(family: string, options: object) {
+    const h = await hash({ family, options })
+    let p = generators[h]
+    if (!p) {
+        const { pipeline } = await import("@huggingface/transformers")
+        p = generators[h] = pipeline("text-generation", family, {
+            ...options,
+            cache_dir: dotGenaiscriptPath("cache", "transformers"),
+            progress_callback: progressBar(),
+        })
+    }
+    return p
 }
 
 export const TransformersCompletion: ChatCompletionHandler = async (
@@ -45,53 +78,74 @@ export const TransformersCompletion: ChatCompletionHandler = async (
     trace
 ) => {
     const { messages, temperature, top_p, max_tokens } = req
-    const { partialCb, inner } = options
+    const { partialCb, inner, cache: cacheOrName, cacheName } = options
     const { model, tag, family } = parseModelIdentifier(req.model)
-    try {
-        trace.startDetails(`transformer`)
-        trace.itemValue("model", model)
 
-        const { pipeline } = await import("@huggingface/transformers")
-        // Create a text generation pipeline
-        const generator = await pipeline("text-generation", family, {
-            dtype: tag as any,
-            cache_dir: dotGenaiscriptPath("cache", "transformers"),
-            progress_callback: progressBar(),
-            device: "cpu",
-        })
-        logVerbose(`transformers model ${model} loaded`)
-        logVerbose(``)
-        const msgs: Chat = chatMessagesToTranformerMessages(messages)
-        trace.detailsFenced("messages", msgs, "yaml")
-        const output = (await generator(
-            msgs,
-            deleteUndefinedValues({
-                max_new_tokens: max_tokens || 4000,
-                temperature,
-                top_p,
-                early_stopping: true,
-            })
-        )) as TextGenerationOutput
-        const text = output
-            .map((msg) => (msg.generated_text.at(-1) as Message).content)
-            .join("")
-        trace.fence(text, "markdown")
+    trace.itemValue("model", model)
+
+    const cache = !!cacheOrName || !!cacheName
+    const cacheStore = getChatCompletionCache(
+        typeof cacheOrName === "string" ? cacheOrName : cacheName
+    )
+    const cachedKey = cache
+        ? <ChatCompletionRequestCacheKey>{
+              ...req,
+              model: req.model,
+              temperature: req.temperature,
+              top_p: req.top_p,
+              max_tokens: req.max_tokens,
+              logit_bias: req.logit_bias,
+          }
+        : undefined
+    trace.itemValue(`caching`, cache)
+    trace.itemValue(`cache`, cacheStore?.name)
+    const { text: cached, finishReason: cachedFinishReason } =
+        (await cacheStore.get(cachedKey)) || {}
+    if (cached !== undefined) {
         partialCb?.({
-            responseSoFar: text,
-            responseChunk: text,
-            tokensSoFar: 0,
+            tokensSoFar: 0, // TODO
+            responseSoFar: cached,
+            responseChunk: cached,
             inner,
         })
-        return {
-            text,
-            finishReason: "stop",
-        } satisfies ChatCompletionResponse
-    } catch (e) {
-        logVerbose(e)
-        throw e
-    } finally {
-        trace.endDetails()
+        trace.itemValue(`cache hit`, await cacheStore.getKeySHA(cachedKey))
+        return { text: cached, finishReason: cachedFinishReason, cached: true }
     }
+
+    const generator = await generationQueue.add(() => loadGenerator(family, {
+        dtype: tag,
+        device: "cpu",
+    }))
+    const msgs: Chat = chatMessagesToTranformerMessages(messages)
+    trace.detailsFenced("messages", msgs, "yaml")
+    const output = (await generator(
+        msgs,
+        deleteUndefinedValues({
+            max_new_tokens: max_tokens || 4000,
+            temperature,
+            top_p,
+            early_stopping: true,
+        })
+    )) as TextGenerationOutput
+    const text = output
+        .map((msg) => (msg.generated_text.at(-1) as Message).content)
+        .join("")
+    trace.fence(text, "markdown")
+    partialCb?.({
+        responseSoFar: text,
+        responseChunk: text,
+        tokensSoFar: 0,
+        inner,
+    })
+
+    const finishReason = "stop"
+    if (finishReason === "stop")
+        await cacheStore.set(cachedKey, { text, finishReason })
+
+    return {
+        text,
+        finishReason: "stop",
+    } satisfies ChatCompletionResponse
 }
 
 // Define the Ollama model with its completion handler and model listing function
