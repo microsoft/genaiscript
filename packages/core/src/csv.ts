57c8e0e2379f6dafbe2ed120eb10e0df8de4e319
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-11-25 05:47:16 +0000

    fix: 🐛 return empty string if CSV is empty

--- packages/core/src/csv.ts
+++ packages/core/src/csv.ts
@@ -86,6 +86,7 @@ export function CSVTryParse(
  * @returns A string representing the CSV formatted data.
  */
 export function CSVStringify(csv: object[], options?: CSVStringifyOptions) {
+    if (!csv) return "" // Return empty string if CSV is empty
     // Convert objects to CSV string using the provided options
     return stringify(csv, options)
 }
