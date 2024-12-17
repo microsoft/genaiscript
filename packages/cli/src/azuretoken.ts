e0aafe1c0404e755d4b6f406111eeae4b651a655
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-12-17 00:12:30 +0000

    feat: enhance token log with type and scopes info ✨

--- packages/cli/src/azuretoken.ts
+++ packages/cli/src/azuretoken.ts
@@ -79,7 +79,7 @@ export async function createAzureToken(
 
     // Log the expiration time of the token
     logVerbose(
-        `azure: token expires at ${new Date(res.expiresOnTimestamp).toLocaleString()}`
+        `azure: ${credentialsType || ""} token (${scopes.join(",")}) expires at ${new Date(res.expiresOnTimestamp).toLocaleString()}`
     )
 
     return res
