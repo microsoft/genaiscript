ad779f11eee12ee6a2d8489fe63f90cfb7e0b19d
pelikhan <pelikhan@users.noreply.github.com>
2024-12-19 04:00:51 +0000

    Deploying to gh-pages from @ microsoft/genaiscript@07f49d15cbf2feed261b3476aa8aaaf8e90689ba 🚀

---
+++ slides/garage-august2024/assets/slidev/default-DtSo7WB4.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-CUO46JCK.js";import{_ as r}from"../index-Fub7ofYQ.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
