f4e086cc2b30eda1e6ac31a4ad0550d0e47f1bea
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-01-07 18:14:23 +0000

    feat: ✨ add MIME types for Markdown and Astro files

--- packages/core/src/mime.ts
+++ packages/core/src/mime.ts
@@ -5,6 +5,8 @@ import mime from "mime"
 export const TYPESCRIPT_MIME_TYPE = "text/x-typescript"
 export const CSHARP_MIME_TYPE = "text/x-csharp"
 export const PYTHON_MIME_TYPE = "text/x-python"
+export const MARKDOWN_MIME_TYPE = "text/markdown"
+export const ASTRO_MIME_TYPE = "text/x-astro"
 
 // Define a function to look up the MIME type for a given filename
 /**
@@ -18,19 +20,10 @@ export const PYTHON_MIME_TYPE = "text/x-python"
  */
 export function lookupMime(filename: string) {
     if (!filename) return "" // Return an empty string if the filename is falsy
-
-    // Check for TypeScript file extension
     if (/\.ts$/i.test(filename)) return TYPESCRIPT_MIME_TYPE
-
-    // Check for C# file extension
     if (/\.cs$/i.test(filename)) return CSHARP_MIME_TYPE
-
-    // Check for Python file extension
     if (/\.py$/i.test(filename)) return PYTHON_MIME_TYPE
-
-    // Check for Astro file extension
-    if (/\.astro$/i.test(filename)) return "text/x-astro"
-
-    // Default to lookup from 'mime-types' or return empty string
+    if (/\.astro$/i.test(filename)) return ASTRO_MIME_TYPE
+    if (/\.prompty$/i.test(filename)) return MARKDOWN_MIME_TYPE
     return mime.getType(filename) || ""
 }
