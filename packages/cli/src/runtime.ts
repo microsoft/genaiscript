8c9456e9c5efb28606dce5b2c657bfd6f1be1b14
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-01-09 16:20:48 +0000

    expose transformers piplie

--- packages/cli/src/runtime.ts
+++ packages/cli/src/runtime.ts
@@ -2,7 +2,8 @@
  * GenAIScript supporting runtime
  */
 import { delay as _delay } from "es-toolkit"
-import { z as zod } from "zod"
+import { z as _z } from "zod"
+import { pipeline as _pipeline } from "@huggingface/transformers"
 
 /**
  * A helper function to delay the execution of the script
@@ -12,4 +13,9 @@ export const delay: (ms: number) => Promise<void> = _delay
 /**
  * Zod schema generator
  */
-export const z = zod
+export const z = _z
+
+/**
+ * HuggingFace transformers.js pipeline apis.
+ */
+export const pipeline = _pipeline
