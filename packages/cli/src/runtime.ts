063a8ac460d2f3a707bcb226ad46ba1a4bbc9a84
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-01-17 07:12:20 +0000

    implement classify prompt in runtime

--- packages/cli/src/runtime.ts
+++ packages/cli/src/runtime.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../core/src/genaisrc/genaiscript.d.ts" />
+
 /**
  * GenAIScript supporting runtime
  */
@@ -19,3 +21,74 @@ export const z = _z
  * HuggingFace transformers.js pipeline apis.
  */
 export const pipeline = _pipeline
+
+/**
+ * Classify prompt
+ * @param text
+ * @param labels
+ * @param options
+ */
+export async function classify(
+    text: string,
+    labels: Record<string, string>,
+    options?: {
+        instructions?: string
+        ctx?: ChatGenerationContext
+    } & Omit<PromptGeneratorOptions, "choices" | "system">
+) {
+    const { instructions, ...rest } = options || {}
+    const entries = Object.entries(labels).map(([k, v]) => [
+        k.trim().toLowerCase(),
+        v,
+    ])
+    const choices = entries.map(([k]) => k)
+    const ctx = options?.ctx || env.generator
+    const res = await ctx.runPrompt(
+        (_) => {
+            _.$`## Expert Classifier
+You are a specialized text classification system. 
+Your task is to carefully read and classify any input text into one (or more) 
+of the predefined labels below. 
+For each label, you will find a short description. 
+Use these descriptions to guide your decision. 
+`.role("system")
+            _.$`## Labels
+You must classify the data as one of the following labels. 
+${entries.map(([id, descr]) => `- Label ${id}: ${descr}`).join("\n")}
+- Label other: This label is used when the text does not fit any of the available labels.
+
+## Output
+Provide a short justification for your choice 
+and output the label as your last word. 
+`
+            _.def("DATA", text)
+            if (options?.instructions) {
+                _.$`## Additional instructions
+
+                ${instructions}                
+                `
+            }
+        },
+        {
+            choices: [...choices, "other"],
+            label: `classify ${choices.join(", ")}`,
+            system: [
+                "system.output_plaintext",
+                "system.safety_jailbreak",
+                "system.safety_harmful_content",
+                "system.safety_protected_material",
+            ],
+            ...rest,
+        }
+    )
+
+    // find the last label
+    const answer = res.text.toLowerCase()
+    const indexes = choices.map((l) => answer.lastIndexOf(l))
+    const labeli = indexes.reduce((previ, label, i) => {
+        if (indexes[i] > indexes[previ]) return i
+        else return previ
+    }, 0)
+    const label = entries[labeli]
+    return label[0]
+}
