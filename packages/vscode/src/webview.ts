a11343addbbe7f6b31350217554501a01c8d0751
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-01-28 18:39:48 +0000

    cleanup

--- packages/vscode/src/webview.ts
+++ packages/vscode/src/webview.ts
@@ -10,7 +10,7 @@ export async function createWebview(
     state: ExtensionState
 ): Promise<vscode.WebviewPanel> {
     const { host, sessionApiKey, context } = state
-    const { url, externalUrl, cspUrl } = await host.server.client()
+    const { externalUrl, cspUrl } = await host.server.client()
 
     const panel = vscode.window.createWebviewPanel(
         TOOL_ID,
