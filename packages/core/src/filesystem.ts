9de8f9f1d8ce23d0022b9037090636db10aa0d6e
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-02-04 16:57:53 +0000

    feat: ✨ ensure destination directory exists before copy

--- packages/core/src/filesystem.ts
+++ packages/core/src/filesystem.ts
@@ -12,6 +12,8 @@ import { JSON5TryParse } from "./json5"
 import { arrayify, dotGenaiscriptPath, logVerbose } from "./util"
 import { XMLTryParse } from "./xml"
 import { YAMLTryParse } from "./yaml"
+import { basename } from "path"
+import { ensureDir } from "fs-extra"
 
 export function createFileSystem(): Omit<
     WorkspaceFileSystem,
@@ -129,6 +131,7 @@ export function createFileSystem(): Omit<
         copyFile: async (src: string, dest: string) => {
             if (DOT_ENV_REGEX.test(src) || DOT_ENV_REGEX.test(dest))
                 throw new Error("copying .env not allowed")
+            await ensureDir(basename(dest))
             await copyFile(src, dest)
         },
     } satisfies Omit<WorkspaceFileSystem, "grep" | "writeCached">
