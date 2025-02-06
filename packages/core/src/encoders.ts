cbf5857026f903933c50703e32da9ece42555e93
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-02-06 16:56:46 +0000

    adding splitter

--- packages/core/src/encoders.ts
+++ packages/core/src/encoders.ts
@@ -6,6 +6,7 @@ import { addLineNumbers, indexToLineNumber } from "./liner"
 import { resolveFileContent } from "./file"
 import type { EncodeOptions } from "gpt-tokenizer/GptEncoding"
 import { assert } from "./util"
+import { TextSplitter } from "./textsplitter"
 
 /**
  * Resolves the appropriate token encoder based on the given model ID.
@@ -94,7 +95,6 @@ export async function chunk(
         ?.toLowerCase()
         ?.replace(/^\./, "")
     const tokenizer = await resolveTokenEncoder(model)
-    const { TextSplitter } = await import("vectra/lib/TextSplitter")
     const ts = new TextSplitter({
         ...rest,
         docType,
