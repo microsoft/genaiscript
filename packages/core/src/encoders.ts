56169628149811abd7c639032396d99780b89a47
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-02-06 17:26:25 +0000

    handle binary files in chunking

--- packages/core/src/encoders.ts
+++ packages/core/src/encoders.ts
@@ -75,10 +75,13 @@ export async function chunk(
     let filename: string
     let content: string
     if (typeof f === "string") {
-        filename = undefined
-        content = f
+        file = {
+            filename: undefined,
+            content: f,
+        }
     } else if (typeof f === "object") {
         await resolveFileContent(f)
+        if (f.encoding) return [] // binary file bail out
         filename = f.filename
         content = f.content
     } else return []
