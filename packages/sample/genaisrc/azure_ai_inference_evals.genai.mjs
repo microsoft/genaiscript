eba9d5531f5d47316580e5a67d9de79a18871e14
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-03-10 17:21:46 +0000

    unwrap error

--- packages/sample/genaisrc/azure_ai_inference_evals.genai.mjs
+++ packages/sample/genaisrc/azure_ai_inference_evals.genai.mjs
@@ -2,9 +2,9 @@ script({
     testModels: [
         "azure_ai_inference:gpt-4o",
         "azure_ai_inference:gpt-4o-mini",
-        "azure_ai_inference:o3-mini",
         "azure_ai_inference:deepseek-r1",
     ],
+    group: "azure_ai_inference",
     tests: [{ files: "src/rag/markdown.md", keywords: "markdown" }],
 })
 
