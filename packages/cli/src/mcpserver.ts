4e722491893d72a2c76984420300ceaeaf79304c
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-03-20 07:35:53 +0000

    use low-level api

--- packages/cli/src/mcpserver.ts
+++ packages/cli/src/mcpserver.ts
@@ -2,42 +2,26 @@ import { TraceOptions } from "../../core/src/trace"
 import { logError, logVerbose, toStringList } from "../../core/src/util"
 import { TOOL_ID } from "../../core/src/constants"
 import { CORE_VERSION } from "../../core/src/version"
-import { Project } from "../../core/src/server/messages"
 import { filterScripts, ScriptFilterOptions } from "../../core/src/ast"
 import z from "zod"
-import { CancellationOptions } from "../../core/src/cancellation"
 import { run } from "./api"
-import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js"
+import { type CallToolResult } from "@modelcontextprotocol/sdk/types.js"
 import { errorMessage } from "../../core/src/error"
 import { buildProject } from "./build"
 
-function promptParameterTypeToZodType(
-    p: PromptParameterType
-): z.ZodType<any, any> {
-    if (typeof p === "string") return z.string().optional()
-    if (typeof p === "number") return z.number().optional()
-    if (typeof p === "boolean") return z.boolean().optional()
-    if (typeof p === "object") {
-        const { type } = p as PromptJSONParameterType<any>
-        if (type === "string") return z.string().optional()
-        if (type === "number") return z.number().optional()
-        if (type === "boolean") return z.boolean().optional()
-    }
-    return z.any().optional()
-}
-
-export async function startMcpServer(options?: ScriptFilterOptions) {
-    const prj = await buildProject() // Build the project to get script templates
-    const scripts = filterScripts(prj.scripts, options)
-
+export async function startMcpServer(
+    options?: ScriptFilterOptions & { sse?: boolean }
+) {
     logVerbose(`mcp server: starting...`)
-    const { McpServer } = await import(
-        "@modelcontextprotocol/sdk/server/mcp.js"
-    )
+    const { Server } = await import("@modelcontextprotocol/sdk/server/index.js")
     const { StdioServerTransport } = await import(
         "@modelcontextprotocol/sdk/server/stdio.js"
     )
-    const server = new McpServer(
+    const { CallToolRequestSchema, ListToolsRequestSchema } = await import(
+        "@modelcontextprotocol/sdk/types.js"
+    )
+
+    const server = new Server(
         {
             name: TOOL_ID,
             version: CORE_VERSION,
@@ -48,63 +32,48 @@ export async function startMcpServer(options?: ScriptFilterOptions) {
             },
         }
     )
-    for (const script of scripts) {
-        registerScriptAsTool(script)
-    }
+    server.setRequestHandler(ListToolsRequestSchema, async (req) => {
+        const prj = await buildProject() // Build the project to get script templates
+        const scripts = filterScripts(prj.scripts, options)
+        const tools = scripts.map((script) => {
+            const { id, title, description, inputSchema } = script
+            return {
+                name: id,
+                description: toStringList(title, description),
+                inputSchema: inputSchema,
+            }
+        })
+        return { tools }
+    })
+    server.setRequestHandler(CallToolRequestSchema, async (req) => {
+        const { name, arguments: args } = req.params
+        try {
+            const { files, ...vars } = args || {}
+            const res = await run(name, files as string[], {
+                vars: vars as Record<string, any>,
+            })
+            return {
+                isError: res.status !== "success" || !!res.error,
+                content: [
+                    {
+                        type: "text",
+                        text: res?.error?.message || res.text,
+                    },
+                ],
+            } satisfies CallToolResult
+        } catch (err) {
+            return {
+                isError: true,
+                content: [
+                    {
+                        type: "text",
+                        text: errorMessage(err),
+                    },
+                ],
+            } satisfies CallToolResult
+        }
+    })
+
     const transport = new StdioServerTransport()
     await server.connect(transport)
-
-    function registerScriptAsTool(script: PromptScript) {
-        const { id, title, description, parameters } = script
-        const args = parametersToZod(parameters)
-        server.tool(
-            id,
-            toStringList(title, description),
-            args,
-            async (args, extra) => {
-                logVerbose(`mcp server: run ${id}...`)
-                try {
-                    const { files, ...params } = args || {}
-                    const res = await run(id, files, {
-                        vars: params,
-                    })
-                    return {
-                        isError: res.status !== "success" || !!res.error,
-                        content: [
-                            {
-                                type: "text",
-                                content: res?.error || res.text,
-                            },
-                        ],
-                    } satisfies CallToolResult
-                } catch (err) {
-                    logError(err)
-                    return {
-                        isError: true,
-                        content: [
-                            {
-                                type: "text",
-                                content: errorMessage(err),
-                            },
-                        ],
-                    } satisfies CallToolResult
-                }
-            }
-        )
-    }
-}
-
-function parametersToZod(parameters: PromptParametersSchema) {
-    if (!parameters) return {}
-    const fields = Object.entries(parameters).map(([name, type]) => [
-        name,
-        Array.isArray(type)
-            ? z.array(promptParameterTypeToZodType(type[0]))
-            : promptParameterTypeToZodType(type),
-    ])
-    const args: z.ZodRawShape = Object.fromEntries([
-        ["files", z.array(z.string()).optional()],
-        ...fields,
-    ])
-    return args
 }
