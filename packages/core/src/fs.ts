1d9a5db85edf3d97595b2397a84d9ef55236f873
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-04-04 05:08:01 +0000

    fix for https://github.com/microsoft/genaiscript/issues/1419

--- packages/core/src/fs.ts
+++ packages/core/src/fs.ts
@@ -7,6 +7,7 @@ import { host } from "./host"
 import { readFile } from "fs/promises"
 import { dirname } from "path"
 import { JSON5TryParse } from "./json5"
+import { homedir } from "os"
 
 /**
  * Changes the file extension of a given file name.
@@ -58,6 +59,17 @@ export async function tryReadText(fn: string) {
 export async function ensureDir(dir: string) {
     dbg(`ensuring directory exists ${dir}`)
     await mkdir(dir, { recursive: true })
+}
+
+/**
+ * Expands homedir
+ */
+export function expandHomeDir(dir: string) {
+    if (dir?.startsWith("~/")) {
+        const home = homedir()
+        dir = host.path.join(home, dir.slice(2))
+    }
+    return dir
 }
 
 /**
