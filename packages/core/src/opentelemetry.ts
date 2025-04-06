e45f53d47c72270a6506a74a5f7a6fccc9311fae
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-04-06 22:50:40 +0000

    configure exporter

--- packages/core/src/opentelemetry.ts
+++ packages/core/src/opentelemetry.ts
@@ -1,8 +1,39 @@
 // debug-otel.ts
 import debug, { Debugger } from "debug"
 import { consoleLogFormat } from "./logging"
+import { genaiscriptDebug } from "./debug"
+const dbg = genaiscriptDebug("otel")
 
-export async function openTelemetryEnable() {
+async function registerOTLPExporter() {
+    const url = process.env.GENAISCRIPT_OPENTELEMETRY_COLLECTOR_URL
+    dbg(`registering OTLP exporter at ${url}`)
+    const { NodeTracerProvider, BatchSpanProcessor } = await import(
+        "@opentelemetry/sdk-trace-node"
+    )
+    const { OTLPTraceExporter } = await import(
+        "@opentelemetry/exporter-trace-otlp-http"
+    )
+    const collectorOptions = {
+        url, // url is optional and can be omitted - default is http://localhost:4318/v1/traces
+        headers: {},
+        concurrencyLimit: 10,
+    }
+    const exporter = new OTLPTraceExporter(collectorOptions)
+    const provider = new NodeTracerProvider({
+        spanProcessors: [
+            new BatchSpanProcessor(exporter, {
+                // The maximum queue size. After the size is reached spans are dropped.
+                maxQueueSize: 1000,
+                // The interval between two consecutive exports
+                scheduledDelayMillis: 30000,
+            }),
+        ],
+    })
+    provider.register()
+}
+
+async function instrumentDebug() {
+    dbg(`instrumenting debug`)
     const { trace, context } = await import("@opentelemetry/api")
 
     // Save the original debug.log function
@@ -26,4 +57,10 @@ export async function openTelemetryEnable() {
             })
         }
     }
+}
+
+export async function openTelemetryEnable() {
+    dbg(`enabling OpenTelemetry`)
+    await registerOTLPExporter()
+    await instrumentDebug()
 }
