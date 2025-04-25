def59e0d91e600dc11f12a27a073842fc7e5e1fa
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-04-25 14:24:57 +0000

    add dall-e-2

--- packages/sample/genaisrc/image-gen.genai.mjs
+++ packages/sample/genaisrc/image-gen.genai.mjs
@@ -1,8 +1,18 @@
-for (const model of ["openai:dall-e-3", "openai:gpt-image-1"]) {
+for (const model of [
+    "openai:dall-e-2",
+    "openai:dall-e-3",
+    "openai:gpt-image-1",
+]) {
     env.output.heading(3, `Model: ${model}`)
     const { image, revisedPrompt } = await generateImage(
         `a cute cat. only one. iconic, high details. 8-bit resolution.`,
-        { maxWidth: 400, autoCrop: true, mime: "image/png", model, size: "square" }
+        {
+            maxWidth: 400,
+            autoCrop: true,
+            mime: "image/png",
+            model,
+            size: "square",
+        }
     )
     env.output.image(image.filename)
     env.output.fence(revisedPrompt)
