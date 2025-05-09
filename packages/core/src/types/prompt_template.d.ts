8389597e058d1c02b5a4fd352fceadd7871f5c18
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-05-09 20:10:50 +0000

    ✨ feat: add parse method to Path interface
    Added parse method for advanced path manipulation and breakdown.

--- packages/core/src/types/prompt_template.d.ts
+++ packages/core/src/types/prompt_template.d.ts
@@ -2165,6 +2165,29 @@ interface RunPromptResult {
  * Path manipulation functions.
  */
 interface Path {
+    parse(path: string): {
+        /**
+         * The root of the path such as '/' or 'c:\'
+         */
+        root: string
+        /**
+         * The full directory path such as '/home/user/dir' or 'c:\path\dir'
+         */
+        dir: string
+        /**
+         * The file name including extension (if any) such as 'index.html'
+         */
+        base: string
+        /**
+         * The file extension (if any) such as '.html'
+         */
+        ext: string
+        /**
+         * The file name without extension (if any) such as 'index'
+         */
+        name: string
+    }
+
     /**
      * Returns the last portion of a path. Similar to the Unix basename command.
      * @param path
