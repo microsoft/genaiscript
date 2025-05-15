93796a9ee13a440dd7cc3c33acb3066483a3417b
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-05-15 14:35:28 +0000

    show ui element when no script

--- packages/sample/genaisrc/add.test.genai.mjs
+++ packages/sample/genaisrc/add.test.genai.mjs
@@ -1,22 +1,23 @@
 script({
-    title: 'Simple Math Test',
-    description: 'Validates that the model correctly calculates 1+1.',
-    group: 'Basic Tests',
-    temperature: 0,  
-    maxTokens: 10, 
+    title: "Simple Math Test",
+    description: "Validates that the model correctly calculates 1+1.",
+    group: "Basic Tests",
+    temperature: 0,
+    maxTokens: 10,
+    accept: "none",
     tests: [
-      {
-        files: [],  
-//        rubrics: ['output correctly calculates 1+1 as 2'],
-//        facts: [`The model should return "2".`],
-        asserts: [
-          {
-            type: 'icontains',
-            value: '2', 
-          },
-        ],
-      },
+        {
+            files: [],
+            //        rubrics: ['output correctly calculates 1+1 as 2'],
+            //        facts: [`The model should return "2".`],
+            asserts: [
+                {
+                    type: "icontains",
+                    value: "2",
+                },
+            ],
+        },
     ],
-  });
-  
-  $`What is 1 + 1?`; 
+})
+
+$`What is 1 + 1?`
