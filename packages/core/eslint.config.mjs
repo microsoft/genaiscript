647ee5a0433c14bcf2fb90930aa3af2342c5d149
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2025-06-30 19:50:54 -0400

    [core] Minor cleanup to core

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -4,4 +4,13 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(
+  eslint.configs.recommended, 
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }
+);
