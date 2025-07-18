d98104119439fee1901377e80cb294618133c485
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-07-18 09:22:51 +0000

    refactor: simplify type exports in index.ts

--- packages/mdx/src/index.ts
+++ packages/mdx/src/index.ts
@@ -1,16 +1,7 @@
 export { MdxCompiler, compileMdx } from "./compiler.js";
 export { PromptDom } from "./dom.js";
 export { MdxTransformer } from "./transformer.js";
-export type {
-  MdxCompilerOptions,
-  MdxCompilerResult,
-  PromptDomNode,
-  SystemProps,
-  UserProps,
-  AssistantProps,
-  DefProps,
-  FileProps
-} from "./types.js";
+export type * from "./types.js";
 
 // Convenience re-export for easy usage
 export { compileMdx as default } from "./compiler.js";
